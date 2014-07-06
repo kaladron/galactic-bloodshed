73c9f3e7b517b71795e3480262dfd211287ec868
Jeff Bailey <jbailey@raspberryginger.com>
2014-07-06 21:27:35 +0000

    Add missing defense.h

---
+++ src/defense.h
@@ -0,0 +1,10 @@
+// Copyright 2014 The Galactic Bloodshed Authors. All rights reserved.
+// Use of this source code is governed by a license that can be
+// found in the COPYING file.
+
+#ifndef DEFENSE_H
+#define DEFENSE_H
+
+extern int Defensedata[];
+
+#endif // DEFENSE_H

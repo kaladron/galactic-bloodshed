0e2bc47205e570454daf1294c49b5623f0651f29
Jeff Bailey <jbailey@raspberryginger.com>
2014-07-07 01:12:38 +0000

    IWYU dock.c

--- src/dock.c
+++ src/dock.c
@@ -1,34 +1,33 @@
-/*
-** Galactic Bloodshed, copyright (c) 1989 by Robert P. Chansky,
-** smq@ucscb.ucsc.edu, mods by people in GB_copyright.h.
-** Restrictions in GB_copyright.h.
-**
-**  dock.c -- dock a ship
-**  and..... assault -- a very un-PC version of dock
-*/
+// Copyright 2014 The Galactic Bloodshed Authors. All rights reserved.
+// Use of this source code is governed by a license that can be
+// found in the COPYING file.
 
-#include "GB_copyright.h"
+/*  dock.c -- dock a ship and..... assault -- a very un-PC version of dock */
+
 #define EXTERN extern
-#include "vars.h"
-#include "ships.h"
-#include "races.h"
-#include "power.h"
-#include "buffers.h"
-#include <signal.h>
+#include "dock.h"
+
 #include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-void dock(int, int, int, int);
 #include "GB_server.h"
-#include "shlmisc.h"
+#include "buffers.h"
+#include "capture.h"
+#include "files.h"
 #include "files_shl.h"
+#include "fire.h"
 #include "getplace.h"
 #include "load.h"
-#include "rand.h"
-#include "capture.h"
-#include "teleg_send.h"
-#include "fire.h"
 #include "max.h"
+#include "races.h"
+#include "rand.h"
+#include "ships.h"
+#include "shlmisc.h"
+#include "teleg_send.h"
+#include "tweakables.h"
+#include "vars.h"
 
 void dock(int Playernum, int Governor, int APcount, int Assault) {
   shiptype *s, *s2, *s3, ship;
--- src/dock.h
+++ src/dock.h
@@ -1,3 +1,10 @@
-/* dock.c function prototypes */
+// Copyright 2014 The Galactic Bloodshed Authors. All rights reserved.
+// Use of this source code is governed by a license that can be
+// found in the COPYING file.
 
-extern void dock(int, int, int, int);
+#ifndef DOCK_H
+#define DOCK_H
+
+void dock(int, int, int, int);
+
+#endif // DOCK_H

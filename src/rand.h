2cda4f61b8c63e940a3fc5061516178b88f4dff5
Jeff Bailey <jbailey@raspberryginger.com>
2014-07-09 06:53:57 +0000

    IWYU rand.c and comment cleanups

--- src/rand.h
+++ src/rand.h
@@ -1,6 +1,13 @@
-/* rand.c function prototypes */
+// Copyright 2014 The Galactic Bloodshed Authors. All rights reserved.
+// Use of this source code is governed by a license that can be
+// found in the COPYING file.
 
-extern double double_rand(void);
-extern int int_rand(int, int);
-extern int round_rand(double);
-extern int rposneg(void);
+#ifndef RAND_H
+#define RAND_H
+
+double double_rand(void);
+int int_rand(int, int);
+int round_rand(double);
+int rposneg(void);
+
+#endif // RAND_H

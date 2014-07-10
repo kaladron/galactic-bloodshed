cf01e67bc58fa8090e0343abe7440f5aa420d9cc
Jeff Bailey <jbailey@raspberryginger.com>
2014-07-10 00:20:01 +0000

    IWYU scrap.c

--- src/scrap.c
+++ src/scrap.c
@@ -1,27 +1,25 @@
-/*
- * Galactic Bloodshed, copyright (c) 1989 by Robert P. Chansky,
- * smq@ucscb.ucsc.edu, mods by people in GB_copyright.h.
- * Restrictions in GB_copyright.h.
- *
- * scrap.c -- turn a ship to junk
- */
+// Copyright 2014 The Galactic Bloodshed Authors. All rights reserved.
+// Use of this source code is governed by a license that can be
+// found in the COPYING file.
 
-#include "GB_copyright.h"
+/* scrap.c -- turn a ship to junk */
+
 #define EXTERN extern
-#include "vars.h"
-#include "ships.h"
-#include "races.h"
-#include "power.h"
-#include "buffers.h"
-#include <signal.h>
+#include "scrap.h"
 
-void scrap(int, int, int);
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "GB_server.h"
-#include "shlmisc.h"
+#include "buffers.h"
 #include "files_shl.h"
 #include "fire.h"
 #include "land.h"
 #include "load.h"
+#include "races.h"
+#include "ships.h"
+#include "shlmisc.h"
+#include "vars.h"
 
 void scrap(int Playernum, int Governor, int APcount) {
   planettype *planet;
--- src/scrap.h
+++ src/scrap.h
@@ -1,3 +1,10 @@
-/* scrap.c function prototypes */
+// Copyright 2014 The Galactic Bloodshed Authors. All rights reserved.
+// Use of this source code is governed by a license that can be
+// found in the COPYING file.
 
-extern void scrap(int, int, int);
+#ifndef SCRAP_H
+#define SCRAP_H
+
+void scrap(int, int, int);
+
+#endif // SCRAP_H

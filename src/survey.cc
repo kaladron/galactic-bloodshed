ef361f9f47a8069696a75bf5c91f34e4e6a72850
Jeff Bailey <jeffbailey@google.com>
2014-08-10 08:45:28 -0700

    get4args is only called by functions in one file, make it a static utility function

--- src/survey.cc
+++ src/survey.cc
@@ -17,7 +17,6 @@
 #include "csp_types.h"
 #include "files_shl.h"
 #include "fire.h"
-#include "get4args.h"
 #include "getplace.h"
 #include "map.h"
 #include "max.h"
@@ -41,6 +40,8 @@ static const char *Tox[] = {
     "Stage 9: WARNING: nearing 100% toxicity",
     "Stage 10: WARNING: COMPLETELY TOXIC!!!",
     "???"};
+
+static void get4args(char *, int *, int *, int *, int *);
 
 void survey(const command_t &argv, const player_t Playernum,
             const governor_t Governor) {
@@ -467,5 +468,29 @@ void repair(const command_t &argv, const player_t Playernum,
   } else {
     sprintf(buf, "scope must be a planet.\n");
     notify(Playernum, Governor, buf);
+  }
+}
+
+static void get4args(char *s, int *xl, int *xh, int *yl, int *yh) {
+  char *p, s1[17], s2[17];
+  p = s;
+
+  sscanf(p, "%[^,]", s1);
+  while ((*p != ':') && (*p != ',')) p++;
+  if (*p == ':') {
+    sscanf(s1, "%d:%d", xl, xh);
+    while (*p != ',') p++;
+  } else if (*p == ',') {
+    sscanf(s1, "%d", xl);
+    *xh = (*xl);
+  }
+
+  sscanf(p, "%s", s2);
+  while ((*p != ':') && (*p != '\0')) p++;
+  if (*p == ':') {
+    sscanf(s2, ",%d:%d", yl, yh);
+  } else {
+    sscanf(s2, ",%d,", yl);
+    *yh = (*yl);
   }
 }

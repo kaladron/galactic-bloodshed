4ac14706bbdc3a5a00dee8b97897139e5693cbd7
Jeff Bailey <jeffbailey@google.com>
2018-11-25 01:07:50 -0500

    Use argv.size instead of global argn.  Add documentation to zoom function

--- src/zoom.cc
+++ src/zoom.cc
@@ -2,7 +2,8 @@
 // Use of this source code is governed by a license that can be
 // found in the COPYING file.
 
-/* zoom.c -- zoom in or out for orbit display */
+/// \file zoom.cc
+/// \brief Functions for implementing the 'zoom' command.
 
 #include "zoom.h"
 
@@ -12,11 +13,12 @@
 #include "buffers.h"
 #include "vars.h"
 
+/// Zoom in or out for orbit display
 void zoom(const command_t &argv, const player_t Playernum,
           const governor_t Governor) {
   int i = (Dir[Playernum - 1][Governor].level == LEVEL_UNIV);
 
-  if (argn > 1) {
+  if (argv.size() > 1) {
     double num, denom;
     if (sscanf(argv[1].c_str(), "%lf/%lf", &num, &denom) == 2) {
       /* num/denom format */

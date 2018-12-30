2105a88627798da2ad2a2ad33da498ae8e0bd86d
Jeff Bailey <jeffbailey@google.com>
2018-12-30 02:37:35 -0500

    Move definition of logscale to the header, delete the now-unused misc.cc file

--- src/misc.h
+++ src/misc.h
@@ -5,6 +5,13 @@
 #ifndef MISC_H
 #define MISC_H
 
-double logscale(int);
+#include <cmath>
+
+/**
+ * \brief Scales used in production efficiency etc.
+ * \param x Integer from 0-100
+ * \return Float 0.0 - 1.0 (logscaleOB 0.5 - .95)
+ */
+inline double logscale(const int x) { return log10((double)x + 1.0) / 2.0; }
 
 #endif  // MISC_H

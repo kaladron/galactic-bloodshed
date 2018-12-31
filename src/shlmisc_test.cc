533fb528a9b8a2258a8771b8e6e2fbc73ab11c54
Jeff Bailey <jeffbailey@google.com>
2018-12-31 15:35:16 -0500

    Add our first unit test.  Sort of.  The code is such a tangled mess that I wound up duplicating the function to be tested, but did this so that I could get the test cases that I'd used to build the function committed.  Detangling will come next.

---
+++ src/shlmisc_test.cc
@@ -0,0 +1,34 @@
+#include <cassert>
+#include <optional>
+#include <string>
+
+#include "shlmisc.h"
+
+// TODO(jeffbailey): Puke.  Detangling the dependencies is too
+// big right now, so this code is duplicated.
+std::optional<shipnum_t> string_to_shipnum(std::string_view s) {
+  if (s.size() > 1 && s[0] == '#') {
+    s.remove_prefix(1);
+    return string_to_shipnum(s);
+  }
+
+  if (s.size() > 0 && std::isdigit(s[0])) {
+    return (std::stoi(std::string(s.begin(), s.end())));
+  }
+  return {};
+}
+
+int main() {
+  auto a = string_to_shipnum("123");
+  assert(*a == 123);
+  auto b = string_to_shipnum("#123");
+  assert(*b == 123);
+  auto c = string_to_shipnum("##123");
+  assert(*c == 123);
+  auto d = string_to_shipnum("abc");
+  assert(!d);
+  auto e = string_to_shipnum("##abc");
+  assert(!e);
+  auto f = string_to_shipnum("");
+  assert(!f);
+}

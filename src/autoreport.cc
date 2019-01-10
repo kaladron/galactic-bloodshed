ab0f0ee2a73e2ec7a00c5dcf94c93dfda4370238
Jeff Bailey <jeffbailey@google.com>
2019-01-10 02:03:38 -0500

    Rework autoreport command.  Add doxygen docs, refactor common code out, with some early escapes for invalid options.  Autoreport used to not work with the parameter (it did the autoreport on the current Planet, and then did it only after the comment was displayed), and now this is fixed.

--- src/autoreport.cc
+++ src/autoreport.cc
@@ -2,14 +2,12 @@
 // Use of this source code is governed by a license that can be
 // found in the COPYING file.
 
-/*
- * autoreport.c -- tell server to generate a report for each planet
- */
+/// \file autoreport.c
+/// \brief Tell server to generate a report for each planet.
 
 #include "autoreport.h"
 
-#include <cstdio>
-#include <cstdlib>
+#include <boost/format.hpp>
 
 #include "GB_server.h"
 #include "buffers.h"
@@ -20,42 +18,46 @@
 #include "vars.h"
 
 void autoreport(const command_t &argv, GameObj &g) {
-  const int Playernum = g.player;
-  const int Governor = g.governor;
-
-  if (Governor && Stars[g.snum]->governor[Playernum - 1] != Governor) {
+  if (g.governor && Stars[g.snum]->governor[g.player - 1] != g.governor) {
     g.out << "You are not authorized to do this here.\n";
     return;
   }
 
-  if (argv.size() == 1) { /* no args */
-    if (g.level == ScopeLevel::LEVEL_PLAN) {
-      auto p = getplanet(g.snum, g.pnum);
-      if (p.info[Playernum - 1].autorep)
-        p.info[Playernum - 1].autorep = 0;
-      else
-        p.info[Playernum - 1].autorep = TELEG_MAX_AUTO;
-      putplanet(p, Stars[g.snum], g.pnum);
+  starnum_t snum;
+  planetnum_t pnum;
 
-      sprintf(buf, "Autoreport on %s has been %s.\n",
-              Stars[g.snum]->pnames[g.pnum],
-              p.info[Playernum - 1].autorep ? "set" : "unset");
-      notify(Playernum, Governor, buf);
-    } else {
-      g.out << "Scope must be a planet.\n";
-    }
-  } else if (argv.size() > 1) { /* argv.size()==2, place specified */
-    auto place = Getplace(g, argv[1], 0);
-    if (place.level == ScopeLevel::LEVEL_PLAN) {
-      auto p = getplanet(g.snum, g.pnum);
-      sprintf(buf, "Autoreport on %s has been %s.\n",
-              Stars[g.snum]->pnames[g.pnum],
-              p.info[Playernum - 1].autorep ? "set" : "unset");
-      notify(Playernum, Governor, buf);
-      p.info[Playernum - 1].autorep = !p.info[Playernum - 1].autorep;
-      putplanet(p, Stars[g.snum], g.pnum);
-    } else {
-      g.out << "Scope must be a planet.\n";
-    }
+  switch (argv.size()) {
+    case 1:
+      if (g.level != ScopeLevel::LEVEL_PLAN) {
+        g.out << "Scope must be a planet.\n";
+        return;
+      }
+      snum = g.snum;
+      pnum = g.pnum;
+      break;
+    case 2: {
+      auto place = Getplace(g, argv[1], 0);
+      if (place.level != ScopeLevel::LEVEL_PLAN) {
+        g.out << "Scope must be a planet.\n";
+        return;
+      }
+      snum = place.snum;
+      pnum = place.pnum;
+    } break;
+    default:
+      g.out << "Invalid number of arguments.\n";
+      return;
   }
+
+  auto p = getplanet(snum, pnum);
+  if (p.info[g.player - 1].autorep) {
+    p.info[g.player - 1].autorep = 0;
+  } else {
+    p.info[g.player - 1].autorep = TELEG_MAX_AUTO;
+  }
+  putplanet(p, Stars[snum], pnum);
+
+  g.out << boost::format("Autoreport on %s has been %s.\n") %
+               Stars[snum]->pnames[pnum] %
+               (p.info[g.player - 1].autorep ? "set" : "unset");
 }

505062e220162107f68d429f8ac190fe36f936b6
Sri Panyam <sri.panyam@gmail.com>
2019-07-31 00:21:02 -0700

    Creators (#4)
    * Taking a first stab at iterators to cleanup how names are generated
    
    * Starting utils folder for common helpers used all around the place
    
    * Copying data files on install
    
    * Adding copyright header
    
    * clang-format

---
+++ gb/creator/namegen.cc
@@ -0,0 +1,28 @@
+// Copyright 2014 The Galactic Bloodshed Authors. All rights reserved.
+// Use of this source code is governed by a license that can be
+// found in the COPYING file.
+
+/**
+ * Different name generator implementations.
+ */
+
+#include "gb/creator/namegen.h"
+#include <sstream>
+
+bool SequentialNameGenerator::next() {
+  std::ostringstream out;
+  out << prefix << currval << suffix;
+  currval++;
+  current_value = out.str();
+  return true;
+}
+
+bool IterativeNameGenerator::next() {
+  current_value = "";
+  if (head == tail) {
+    return false;
+  }
+  current_value = *head;
+  ++head;
+  return true;
+}
---
+++ gb/utils/fileutils.h
@@ -0,0 +1,22 @@
+// Copyright 2014 The Galactic Bloodshed Authors. All rights reserved.
+// Use of this source code is governed by a license that can be
+// found in the COPYING file.
+
+#ifndef FILEUTILS_H
+#define FILEUTILS_H
+
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+
+#include <sys/stat.h>
+#include <unistd.h>
+
+/**
+ * Helper utils and classes for making common operations with files eaiser.
+ */
+void InitFile(const std::string &path, void *buffer = nullptr,
+              size_t length = 0);
+void EmptyFile(const std::string &path);
+
+#endif  // MAKEPLANET_H

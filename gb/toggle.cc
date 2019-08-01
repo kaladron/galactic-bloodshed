f60156f3bd77cdc3eca367e36c24354ded2b2c3f
Jeff Bailey <jbailey@raspberryginger.com>
2019-08-01 00:49:02 -0700

    Split out highlight command

--- gb/toggle.cc
+++ gb/toggle.cc
@@ -98,19 +98,3 @@ void toggle(const command_t &argv, GameObj &g) {
     }
   }
 }
-
-void highlight(const command_t &argv, GameObj &g) {
-  player_t Playernum = g.player;
-  governor_t Governor = g.governor;
-  // TODO(jeffbailey): int APcount = 0;
-  player_t n;
-  racetype *Race;
-
-  if (!(n = get_player(argv[1]))) {
-    g.out << "No such player.\n";
-    return;
-  }
-  Race = races[Playernum - 1];
-  Race->governor[Governor].toggle.highlight = n;
-  putrace(Race);
-}

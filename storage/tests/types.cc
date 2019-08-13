ed4f754eb69842870fa70aa571e95052df97fd9e
Sri Panyam <sri.panyam@gmail.com>
2019-08-13 08:28:54 +0000

    Adding dummy tests, verbose check for tests

--- storage/tests/types.cc
+++ storage/tests/types.cc
@@ -2,4 +2,17 @@
 #include <GUnit.h>
 
 GTEST("Calc Test") {
+    SHOULD("return sum of 2 numbers") {
+    }
+
+    SHOULD("return sum of 3 numbers") {
+    }
+}
+
+GTEST("Calc Test 2") {
+    SHOULD("return sum of 2 numbers") {
+    }
+
+    SHOULD("return sum of 3 numbers") {
+    }
 }

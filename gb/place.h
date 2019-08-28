3b25bcf2c002ce40a1d6bb6bbd00c50a62ec9c58
Jeff Bailey <jbailey@raspberryginger.com>
2019-08-28 00:38:03 -0700

    Split Place class into its own files in prep for turning it into a real class

---
+++ gb/place.h
@@ -0,0 +1,21 @@
+// Copyright 2019 The Galactic Bloodshed Authors. All rights reserved.
+// Use of this source code is governed by a license that can be
+// found in the COPYING file.
+
+#ifndef PLACE_H
+#define PLACE_H
+
+#include "gb/vars.h"
+
+class Place { /* used in function return for finding place */
+ public:
+  starnum_t snum;
+  planetnum_t pnum;
+  shipnum_t shipno;
+  ScopeLevel level;      /* .level */
+  unsigned char err = 0; /* if error */
+};
+
+std::string Dispplace(const Place &);
+
+#endif  // PLACE_H

319b088ba8eebb0e377f3dfec9ce5430abbba1de
Jeff Bailey <jbailey@raspberryginger.com>
2019-10-01 18:24:25 -0700

    Move Sector to gblib

--- gb/vars.h
+++ gb/vars.h
@@ -135,52 +135,6 @@ class Planet {
   planetnum_t planet_id;
 };
 
-class Sector {
- public:
-  Sector(int x_, int y_, int eff_, int fert_, int mobilization_, int crystals_,
-         int resource_, int popn_, int troops_, int owner_, int race_,
-         int type_, int condition_)
-      : x(x_),
-        y(y_),
-        eff(eff_),
-        fert(fert_),
-        mobilization(mobilization_),
-        crystals(crystals_),
-        resource(resource_),
-        popn(popn_),
-        troops(troops_),
-        owner(owner_),
-        race(race_),
-        type(type_),
-        condition(condition_) {}
-
-  Sector() = default;
-  Sector(Sector &) = delete;
-  void operator=(const Sector &) = delete;
-  Sector(Sector &&) = default;
-  Sector &operator=(Sector &&) = default;
-
-  unsigned int x{0};
-  unsigned int y{0};
-  unsigned int eff{0};          /* efficiency (0-100) */
-  unsigned int fert{0};         /* max popn is proportional to this */
-  unsigned int mobilization{0}; /* percent popn is mobilized for war */
-  unsigned int crystals{0};
-  resource_t resource{0};
-
-  population_t popn{0};
-  population_t troops{0}; /* troops (additional combat value) */
-
-  player_t owner{0};         /* owner of place */
-  player_t race{0};          /* race type occupying sector
-                 (usually==owner) - makes things more
-                 realistic when alien races revolt and
-                 you gain control of them! */
-  unsigned int type{0};      /* underlying sector geology */
-  unsigned int condition{0}; /* environmental effects */
-  friend std::ostream &operator<<(std::ostream &, const Sector &);
-};
-
 class SectorMap {
  public:
   SectorMap(const Planet &planet) : maxx_(planet.Maxx), maxy_(planet.Maxy) {

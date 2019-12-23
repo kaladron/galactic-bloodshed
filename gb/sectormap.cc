079f84f09f36484221de173663c5c31e8a3a9f1c
Jeff Bailey <jbailey@raspberryginger.com>
2019-12-23 12:44:45 -0800

    Add shuffle function that adds a permuted vector of sectors.  The interface is terrible, but this helps me start getting rid of some uglier functions that relied on static memory

--- gb/sectormap.cc
+++ gb/sectormap.cc
@@ -11,3 +11,14 @@ import gblib;
 Sector &SectorMap::get_random() {
   return get(int_rand(0, maxx_ - 1), int_rand(0, maxy_ - 1));
 }
+
+std::vector<std::reference_wrapper<Sector>> SectorMap::shuffle() {
+  std::random_device rd;
+  std::default_random_engine g(rd());
+
+  std::vector<std::reference_wrapper<Sector>> shuffled(vec_.begin(),
+                                                       vec_.end());
+  std::shuffle(shuffled.begin(), shuffled.end(), g);
+
+  return shuffled;
+}

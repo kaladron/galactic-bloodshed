23a06206012bd3a9b1832c7f664487586f5f5736
Jeff Bailey <jbailey@raspberryginger.com>
2019-12-29 11:37:48 -0800

    Move survey.cc into the commands subdir and split out repair function into repair.cc

---
+++ gb/commands/repair.cc
@@ -0,0 +1,130 @@
+// Copyright 2014 The Galactic Bloodshed Authors. All rights reserved.
+// Use of this source code is governed by a license that can be
+// found in the COPYING file.
+
+import gblib;
+import std;
+
+#include "gb/commands/repair.h"
+
+#include "gb/GB_server.h"
+#include "gb/buffers.h"
+#include "gb/csp.h"
+#include "gb/csp_types.h"
+#include "gb/files_shl.h"
+#include "gb/fire.h"
+#include "gb/getplace.h"
+#include "gb/map.h"
+#include "gb/max.h"
+#include "gb/races.h"
+#include "gb/ships.h"
+#include "gb/tweakables.h"
+#include "gb/vars.h"
+
+namespace {
+void get4args(const char *s, int *xl, int *xh, int *yl, int *yh) {
+  char s1[17];
+  char s2[17];
+  const char *p = s;
+
+  sscanf(p, "%[^,]", s1);
+  while ((*p != ':') && (*p != ',')) p++;
+  if (*p == ':') {
+    sscanf(s1, "%d:%d", xl, xh);
+    while (*p != ',') p++;
+  } else if (*p == ',') {
+    sscanf(s1, "%d", xl);
+    *xh = (*xl);
+  }
+
+  sscanf(p, "%s", s2);
+  while ((*p != ':') && (*p != '\0')) p++;
+  if (*p == ':') {
+    sscanf(s2, ",%d:%d", yl, yh);
+  } else {
+    sscanf(s2, ",%d,", yl);
+    *yh = (*yl);
+  }
+}
+}  // namespace
+
+void repair(const command_t &argv, GameObj &g) {
+  const player_t Playernum = g.player;
+  const governor_t Governor = g.governor;
+  int lowx;
+  int hix;
+  int lowy;
+  int hiy;
+  int x2;
+  int sectors;
+  int cost;
+
+  std::unique_ptr<Place> where;
+  if (argv.size() == 1) { /* no args */
+    where = std::make_unique<Place>(g.level, g.snum, g.pnum);
+  } else {
+    /* repairing a sector */
+    if (isdigit(argv[1][0]) && index(argv[1].c_str(), ',') != nullptr) {
+      if (g.level != ScopeLevel::LEVEL_PLAN) {
+        sprintf(buf, "There are no sectors here.\n");
+        notify(Playernum, Governor, buf);
+        return;
+      }
+      where = std::make_unique<Place>(ScopeLevel::LEVEL_PLAN, g.snum, g.pnum);
+
+    } else {
+      where = std::make_unique<Place>(g, argv[1]);
+      if (where->err || where->level == ScopeLevel::LEVEL_SHIP) return;
+    }
+  }
+
+  if (where->level == ScopeLevel::LEVEL_PLAN) {
+    auto p = getplanet(where->snum, where->pnum);
+    if (!p.info[Playernum - 1].numsectsowned) {
+      notify(Playernum, Governor,
+             "You don't own any sectors on this planet.\n");
+      return;
+    }
+    auto smap = getsmap(p);
+    if (isdigit(argv[1][0]) && index(argv[1].c_str(), ',') != nullptr) {
+      get4args(argv[1].c_str(), &x2, &hix, &lowy, &hiy);
+      /* ^^^ translate from lowx:hix,lowy:hiy */
+      x2 = std::max(0, x2);
+      hix = std::min(hix, p.Maxx - 1);
+      lowy = std::max(0, lowy);
+      hiy = std::min(hiy, p.Maxy - 1);
+    } else {
+      /* repair entire planet */
+      x2 = 0;
+      hix = p.Maxx - 1;
+      lowy = 0;
+      hiy = p.Maxy - 1;
+    }
+    sectors = 0;
+    cost = 0;
+
+    for (; lowy <= hiy; lowy++)
+      for (lowx = x2; lowx <= hix; lowx++) {
+        if (p.info[Playernum - 1].resource >= SECTOR_REPAIR_COST) {
+          auto &s = smap.get(lowx, lowy);
+          if (s.condition == SectorType::SEC_WASTED &&
+              (s.owner == Playernum || !s.owner)) {
+            s.condition = s.type;
+            s.fert = std::min(100U, s.fert + 20);
+            p.info[Playernum - 1].resource -= SECTOR_REPAIR_COST;
+            cost += SECTOR_REPAIR_COST;
+            sectors += 1;
+            putsector(s, p, lowx, lowy);
+          }
+        }
+      }
+    putplanet(p, Stars[where->snum], where->pnum);
+
+    sprintf(buf, "%d sectors repaired at a cost of %d resources.\n", sectors,
+            cost);
+    notify(Playernum, Governor, buf);
+  } else {
+    sprintf(buf, "scope must be a planet.\n");
+    notify(Playernum, Governor, buf);
+  }
+}
--- gb/commands/survey.cc
+++ gb/commands/survey.cc
@@ -7,7 +7,7 @@
 import gblib;
 import std;
 
-#include "gb/survey.h"
+#include "gb/commands/survey.h"
 
 #include "gb/GB_server.h"
 #include "gb/buffers.h"
@@ -39,7 +39,32 @@ static const char *Tox[] = {
     "Stage 10: WARNING: COMPLETELY TOXIC!!!",
     "???"};
 
-static void get4args(const char *, int *, int *, int *, int *);
+namespace {
+void get4args(const char *s, int *xl, int *xh, int *yl, int *yh) {
+  char s1[17];
+  char s2[17];
+  const char *p = s;
+
+  sscanf(p, "%[^,]", s1);
+  while ((*p != ':') && (*p != ',')) p++;
+  if (*p == ':') {
+    sscanf(s1, "%d:%d", xl, xh);
+    while (*p != ',') p++;
+  } else if (*p == ',') {
+    sscanf(s1, "%d", xl);
+    *xh = (*xl);
+  }
+
+  sscanf(p, "%s", s2);
+  while ((*p != ':') && (*p != '\0')) p++;
+  if (*p == ':') {
+    sscanf(s2, ",%d:%d", yl, yh);
+  } else {
+    sscanf(s2, ",%d,", yl);
+    *yh = (*yl);
+  }
+}
+}  // namespace
 
 void survey(const command_t &argv, GameObj &g) {
   const player_t Playernum = g.player;
@@ -369,109 +394,3 @@ void survey(const command_t &argv, GameObj &g) {
     g.out << "Illegal scope.\n";
   }
 } /* end survey */
-
-void repair(const command_t &argv, GameObj &g) {
-  const player_t Playernum = g.player;
-  const governor_t Governor = g.governor;
-  int lowx;
-  int hix;
-  int lowy;
-  int hiy;
-  int x2;
-  int sectors;
-  int cost;
-
-  std::unique_ptr<Place> where;
-  if (argv.size() == 1) { /* no args */
-    where = std::make_unique<Place>(g.level, g.snum, g.pnum);
-  } else {
-    /* repairing a sector */
-    if (isdigit(argv[1][0]) && index(argv[1].c_str(), ',') != nullptr) {
-      if (g.level != ScopeLevel::LEVEL_PLAN) {
-        sprintf(buf, "There are no sectors here.\n");
-        notify(Playernum, Governor, buf);
-        return;
-      }
-      where = std::make_unique<Place>(ScopeLevel::LEVEL_PLAN, g.snum, g.pnum);
-
-    } else {
-      where = std::make_unique<Place>(g, argv[1]);
-      if (where->err || where->level == ScopeLevel::LEVEL_SHIP) return;
-    }
-  }
-
-  if (where->level == ScopeLevel::LEVEL_PLAN) {
-    auto p = getplanet(where->snum, where->pnum);
-    if (!p.info[Playernum - 1].numsectsowned) {
-      notify(Playernum, Governor,
-             "You don't own any sectors on this planet.\n");
-      return;
-    }
-    auto smap = getsmap(p);
-    if (isdigit(argv[1][0]) && index(argv[1].c_str(), ',') != nullptr) {
-      get4args(argv[1].c_str(), &x2, &hix, &lowy, &hiy);
-      /* ^^^ translate from lowx:hix,lowy:hiy */
-      x2 = std::max(0, x2);
-      hix = std::min(hix, p.Maxx - 1);
-      lowy = std::max(0, lowy);
-      hiy = std::min(hiy, p.Maxy - 1);
-    } else {
-      /* repair entire planet */
-      x2 = 0;
-      hix = p.Maxx - 1;
-      lowy = 0;
-      hiy = p.Maxy - 1;
-    }
-    sectors = 0;
-    cost = 0;
-
-    for (; lowy <= hiy; lowy++)
-      for (lowx = x2; lowx <= hix; lowx++) {
-        if (p.info[Playernum - 1].resource >= SECTOR_REPAIR_COST) {
-          auto &s = smap.get(lowx, lowy);
-          if (s.condition == SectorType::SEC_WASTED &&
-              (s.owner == Playernum || !s.owner)) {
-            s.condition = s.type;
-            s.fert = std::min(100U, s.fert + 20);
-            p.info[Playernum - 1].resource -= SECTOR_REPAIR_COST;
-            cost += SECTOR_REPAIR_COST;
-            sectors += 1;
-            putsector(s, p, lowx, lowy);
-          }
-        }
-      }
-    putplanet(p, Stars[where->snum], where->pnum);
-
-    sprintf(buf, "%d sectors repaired at a cost of %d resources.\n", sectors,
-            cost);
-    notify(Playernum, Governor, buf);
-  } else {
-    sprintf(buf, "scope must be a planet.\n");
-    notify(Playernum, Governor, buf);
-  }
-}
-
-static void get4args(const char *s, int *xl, int *xh, int *yl, int *yh) {
-  char s1[17];
-  char s2[17];
-  const char *p = s;
-
-  sscanf(p, "%[^,]", s1);
-  while ((*p != ':') && (*p != ',')) p++;
-  if (*p == ':') {
-    sscanf(s1, "%d:%d", xl, xh);
-    while (*p != ',') p++;
-  } else if (*p == ',') {
-    sscanf(s1, "%d", xl);
-    *xh = (*xl);
-  }
-
-  sscanf(p, "%s", s2);
-  while ((*p != ':') && (*p != '\0')) p++;
-  if (*p == ':') {
-    sscanf(s2, ",%d:%d", yl, yh);
-  } else {
-    sscanf(s2, ",%d,", yl);
-    *yh = (*yl);
-  }
-}

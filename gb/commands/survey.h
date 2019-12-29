9f3831f7232ba6daff40ab7e5e4c40851b50ebba
Jeff Bailey <jbailey@raspberryginger.com>
2019-12-29 11:37:48 -0800

    Move survey.cc into the commands subdir and split out repair function into repair.cc

--- gb/commands/survey.h
+++ gb/commands/survey.h
@@ -1,13 +1,12 @@
-// Copyright 2014 The Galactic Bloodshed Authors. All rights reserved.
+// Copyright 2019 The Galactic Bloodshed Authors. All rights reserved.
 // Use of this source code is governed by a license that can be
 // found in the COPYING file.
 
-#ifndef SURVEY_H
-#define SURVEY_H
+#ifndef CMDS_SURVEY_H
+#define CMDS_SURVEY_H
 
 #include "gb/vars.h"
 
 void survey(const command_t &, GameObj &);
-void repair(const command_t &, GameObj &);
 
 #endif  // SURVEY_H

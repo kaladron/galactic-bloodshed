66ace514f0391d98ec7aa3b14efa93debabf4d2e
Jeff Bailey <jbailey@raspberryginger.com>
2020-01-01 02:14:39 -0800

    Clean up star_locations with local declarations/definitions, removing unneeded temporaries

--- gb/commands/star_locations.cc
+++ gb/commands/star_locations.cc
@@ -9,40 +9,21 @@ import std;
 
 #include "gb/GB_server.h"
 #include "gb/buffers.h"
-#include "gb/files_shl.h"
-#include "gb/map.h"
-#include "gb/max.h"
-#include "gb/place.h"
-#include "gb/power.h"
-#include "gb/races.h"
-#include "gb/ships.h"
-#include "gb/tech.h"
-#include "gb/tweakables.h"
-#include "gb/vars.h"
 
 void star_locations(const command_t &argv, GameObj &g) {
-  const player_t Playernum = g.player;
-  const governor_t Governor = g.governor;
-  int i;
-  double dist;
-  double x;
-  double y;
   int max;
-
-  x = g.lastx[1];
-  y = g.lasty[1];
-
   if (argv.size() > 1)
     max = std::stoi(argv[1]);
   else
     max = 999999;
 
-  for (i = 0; i < Sdata.numstars; i++) {
-    dist = sqrt(Distsq(Stars[i]->xpos, Stars[i]->ypos, x, y));
-    if ((int)dist <= max) {
+  for (auto i = 0; i < Sdata.numstars; i++) {
+    auto dist =
+        sqrt(Distsq(Stars[i]->xpos, Stars[i]->ypos, g.lastx[1], g.lasty[1]));
+    if (std::floor(dist) <= max) {
       sprintf(buf, "(%2d) %20.20s (%8.0f,%8.0f) %7.0f\n", i, Stars[i]->name,
               Stars[i]->xpos, Stars[i]->ypos, dist);
-      notify(Playernum, Governor, buf);
+      notify(g.player, g.governor, buf);
     }
   }
 }

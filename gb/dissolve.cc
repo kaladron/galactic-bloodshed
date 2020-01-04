c3641c069a85db93071c454b4729edc48d8e8f91
Jeff Bailey <jbailey@raspberryginger.com>
2020-01-03 22:54:51 -0800

    Move revolt function to name.cc, the only place it's used.  Move it to an anonymous namespace.

--- gb/dissolve.cc
+++ gb/dissolve.cc
@@ -125,31 +125,3 @@ void dissolve(const command_t &argv, GameObj &g) {
 #endif
 }
 
-int revolt(Planet &pl, const player_t victim, const player_t agent) {
-  int revolted_sectors = 0;
-
-  auto smap = getsmap(pl);
-  for (auto &s : smap) {
-    if (s.owner != victim || s.popn == 0) continue;
-
-    // Revolt rate is a function of tax rate.
-    if (!success(pl.info[victim - 1].tax)) continue;
-
-    if (static_cast<unsigned long>(long_rand(1, s.popn)) <=
-        10 * races[victim - 1]->fighters * s.troops)
-      continue;
-
-    // Revolt successful.
-    s.owner = agent;                   /* enemy gets it */
-    s.popn = int_rand(1, (int)s.popn); /* some people killed */
-    s.troops = 0;                      /* all troops destroyed */
-    pl.info[victim - 1].numsectsowned -= 1;
-    pl.info[agent - 1].numsectsowned += 1;
-    pl.info[victim - 1].mob_points -= s.mobilization;
-    pl.info[agent - 1].mob_points += s.mobilization;
-    revolted_sectors++;
-  }
-  putsmap(smap, pl);
-
-  return revolted_sectors;
-}

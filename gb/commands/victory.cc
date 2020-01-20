089ea7f53cdd993ecc258ecabdf6868209b5ed63
Jeff Bailey <jbailey@raspberryginger.com>
2020-01-19 21:00:29 -0800

    Use range-for loop for displaying victories instead of array offsets.

--- gb/commands/victory.cc
+++ gb/commands/victory.cc
@@ -17,21 +17,20 @@ void victory(const command_t &argv, GameObj &g) {
   int count = (argv.size() > 1) ? std::stoi(argv[1]) : Num_races;
   if (count > Num_races) count = Num_races;
 
-  auto vic = create_victory_list();
+  auto viclist = create_victory_list();
 
   g.out << "----==== PLAYER RANKINGS ====----\n";
   sprintf(buf, "%-4.4s %-15.15s %8s\n", "No.", "Name", (g.god ? "Score" : ""));
   notify(g.player, g.governor, buf);
-  for (int i = 0; i < count; i++) {
+  for (int i = 0; auto &vic : viclist) {
+    i++;
     if (g.god)
-      sprintf(buf, "%2d %c [%2d] %-15.15s %5ld  %6.2f %3d %s %s\n", i + 1,
-              vic[i].Thing ? 'M' : ' ', vic[i].racenum, vic[i].name.c_str(),
-              vic[i].rawscore, vic[i].tech, vic[i].IQ,
-              races[vic[i].racenum - 1].password,
-              races[vic[i].racenum - 1].governor[0].password);
+      sprintf(buf, "%2d %c [%2d] %-15.15s %5ld  %6.2f %3d %s %s\n", i,
+              vic.Thing ? 'M' : ' ', vic.racenum, vic.name.c_str(),
+              vic.rawscore, vic.tech, vic.IQ, races[vic.racenum - 1].password,
+              races[vic.racenum - 1].governor[0].password);
     else
-      sprintf(buf, "%2d   [%2d] %-15.15s\n", i + 1, vic[i].racenum,
-              vic[i].name.c_str());
+      sprintf(buf, "%2d   [%2d] %-15.15s\n", i, vic.racenum, vic.name.c_str());
     notify(g.player, g.governor, buf);
   }
 }

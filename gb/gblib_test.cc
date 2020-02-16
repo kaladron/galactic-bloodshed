35e23e7eb63d837bf1edea5d8b605a3699fc083d
Jeff Bailey <jbailey@raspberryginger.com>
2020-02-15 17:11:14 -0800

    Add first gblib test, testing bitwise operations

---
+++ gb/gblib_test.cc
@@ -0,0 +1,19 @@
+import gblib;
+import std;
+
+#include <cassert>
+
+int main() {
+  uint8_t test8 = 0;
+  uint32_t test32 = 0;
+  uint64_t test64 = 0;
+
+  setbit(test8, 4U);
+  assert(test8 == 16);
+
+  setbit(test32, 22U);
+  assert(test32 == 4194304);
+
+  setbit(test64, 48U);
+  assert(test64 == exp2(48));
+}

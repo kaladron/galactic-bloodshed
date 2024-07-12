3eb90ed71100d297aec83ce81f3da21ac592a68b
Jeff Bailey <jeffbailey@google.com>
2024-07-12 09:18:20 +0000

    Remove unneeded includes.

--- gb/order.cc
+++ gb/order.cc
@@ -11,9 +11,7 @@ import std.compat;
 
 #include "gb/GB_server.h"
 #include "gb/buffers.h"
-#include "gb/build.h"
 #include "gb/files_shl.h"
-#include "gb/fire.h"
 #include "gb/load.h"
 #include "gb/moveship.h"
 #include "gb/place.h"

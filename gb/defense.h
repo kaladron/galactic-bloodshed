902ef7c73434971877976b274085d488c76517d6
Jeff Bailey <jeffbailey@google.com>
2024-07-16 16:45:29 +0000

    Use inline constexpr for Defensedata
    Also, document what it is, the prior comment just refers to an old .S file that isn't present.

--- gb/defense.h
+++ gb/defense.h
@@ -5,6 +5,7 @@
 #ifndef DEFENSE_H
 #define DEFENSE_H
 
-extern const int Defensedata[];
+// These map to SectorType and give the natural defenses for each type of sector.
+inline constexpr std::array<int, 9> Defensedata = {1, 1, 3, 2, 2, 3, 2, 4, 0};
 
 #endif  // DEFENSE_H

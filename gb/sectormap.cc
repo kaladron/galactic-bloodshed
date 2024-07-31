9464130432edb6d234d99fe70700b468b45adf7a
Jeff Bailey <jeffbailey@google.com>
2024-07-31 09:40:14 +0000

    Move SectorMap to module

--- gb/sectormap.cc
+++ gb/sectormap.cc
@@ -1,11 +1,10 @@
-// Copyright 2014 The Galactic Bloodshed Authors. All rights reserved.
-// Use of this source code is governed by a license that can be
-// found in the COPYING file.
+// SPDX-License-Identifier: Apache-2.0
+
+module;
 
 import std.compat;
-import gblib;
 
-#include "gb/vars.h"
+module gblib;
 
 Sector &SectorMap::get_random() {
   return get(int_rand(0, maxx_ - 1), int_rand(0, maxy_ - 1));
--- gb/vars.h
+++ gb/vars.h
@@ -18,44 +18,4 @@
 #define Resources(x) ((x) & M_RESOURCES)
 #define Crystals(x) ((x) & M_CRYSTALS)
 
-class SectorMap {
- public:
-  SectorMap(const Planet &planet) : maxx_(planet.Maxx), maxy_(planet.Maxy) {
-    vec_.reserve(planet.Maxx * planet.Maxy);
-  }
-
-  //! Add an empty sector for every potential space.  Used for initialization.
-  SectorMap(const Planet &planet, bool)
-      : maxx_(planet.Maxx),
-        maxy_(planet.Maxy),
-        vec_(planet.Maxx * planet.Maxy) {}
-
-  // TODO(jeffbailey): Should wrap this in a subclass so the underlying
-  // vector isn't exposed to callers.
-  auto begin() { return vec_.begin(); }
-  auto end() { return vec_.end(); }
-
-  Sector &get(const int x, const int y) {
-    return vec_.at(static_cast<size_t>(x + (y * maxx_)));
-  }
-  void put(Sector &&s) { vec_.emplace_back(std::move(s)); }
-  int get_maxx() { return maxx_; }
-  int get_maxy() { return maxy_; }
-  Sector &get_random();
-  // TODO(jeffbailey): Don't expose the underlying vector.
-  std::vector<std::reference_wrapper<Sector>>
-  shuffle();  /// Randomizes the order of the SectorMap.
-
-  SectorMap(SectorMap &) = delete;
-  void operator=(const SectorMap &) = delete;
-  SectorMap(SectorMap &&) = default;
-  SectorMap &operator=(SectorMap &&) = default;
-
- private:
-  SectorMap(const int maxx, const int maxy) : maxx_(maxx), maxy_(maxy) {}
-  int maxx_;
-  int maxy_;
-  std::vector<Sector> vec_;
-};
-
 #endif  // VARS_H

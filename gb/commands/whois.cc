9303f351da4e59b77759dcd924083c6d060f9cee
Jeff Bailey <jeffbailey@google.com>
2024-08-09 08:27:21 +0000

    [whois] General cleanup.
    Remove use of old-style buffers.
    Remove use of notify.
    Remove unneeded numraces variable.
    Use a ranged for loop instead of subscripts for argv.
    Eliminate a couple of unneeded temporaries.

--- gb/commands/whois.cc
+++ gb/commands/whois.cc
@@ -5,35 +5,34 @@ module;
 import gblib;
 import std.compat;
 
-#include "gb/buffers.h"
-
 module commands;
 
 namespace GB::commands {
 void whois(const command_t &argv, GameObj &g) {
-  player_t Playernum = g.player;
-  governor_t Governor = g.governor;
   // TODO(jeffbailey): ap_t APcount = 0;
 
   if (argv.size() <= 1) {
-    whois({"whois", std::to_string(Playernum)}, g);
+    whois({"whois", std::to_string(g.player)}, g);
     return;
   }
-  auto numraces = Num_races;
 
-  for (size_t i = 1; i <= argv.size() - 1; i++) {
-    auto j = std::stoi(argv[i]);
-    if (!(j < 1 || j > numraces)) {
-      auto &race = races[j - 1];
-      if (j == Playernum)
-        sprintf(buf, "[%2d, %d] %s \"%s\"\n", j, Governor, race.name,
-                race.governor[Governor].name);
-      else
-        sprintf(buf, "[%2d] %s\n", j, race.name);
-    } else {
-      sprintf(buf, "Identify: Invalid player number #%d. Try again.\n", j);
+  for (const auto &player :
+       std::ranges::subrange(argv.begin() + 1, argv.end())) {
+    const auto j = std::stoi(player);
+
+    if (j < 1 || j > Num_races) {
+      g.out << std::format("Identify: Invalid player number #{}. Try again.\n",
+                           j);
+      continue;
     }
-    notify(Playernum, Governor, buf);
+
+    const auto &race = races[j - 1];
+    if (j == g.player) {
+      g.out << std::format("[{:2d}, {}] {} \"{}\"\n", j, g.governor, race.name,
+                           race.governor[g.governor].name);
+    } else {
+      g.out << std::format("[{:2d}] {}\n", j, race.name);
+    }
   }
 }
 }  // namespace GB::commands

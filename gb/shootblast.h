88b8de3141ccc052d0d04c3e0314fd3865b7e2fd
Jeff Bailey <jeffbailey@google.com>
2024-08-20 06:48:10 +0000

    Fill in all the parameter names in shootblase header file.

--- gb/shootblast.h
+++ gb/shootblast.h
@@ -5,16 +5,41 @@
 #ifndef SHOOTBLAST_H
 #define SHOOTBLAST_H
 
-int shoot_ship_to_ship(Ship *, Ship *, int, int, int, char *, char *);
-int shoot_planet_to_ship(Race &, Ship *, int strength, char *long_msg,
+int shoot_ship_to_ship(Ship *attacker, Ship *target, int strength, int range,
+                       int accuracy, char *long_msg, char *short_msg);
+int shoot_planet_to_ship(Race &race, Ship *target, int strength, char *long_msg,
                          char *short_msg);
-int shoot_ship_to_planet(Ship *, Planet &, int, int, int, SectorMap &, int, int,
-                         char *, char *);
-int hit_odds(double, int *, double, int, int, int, int, int, int, int, int);
-double gun_range(Race *, Ship *, int);
-double tele_range(int, double);
-int current_caliber(Ship *);
-void do_collateral(Ship *, int, int *, int *, int *, int *);
-int planet_guns(long);
+int shoot_ship_to_planet(Ship *attacker, Planet &target, int strength,
+                         int range, int accuracy, SectorMap &sector_map,
+                         int sector_x, int sector_y, char *long_msg,
+                         char *short_msg);
+int hit_odds(double attacker_strength, int *attacker_guns,
+             double target_strength, int target_shields, int target_defense,
+             int target_damage, int target_crew, int target_hull,
+             int target_engine, int target_fuel, int target_mass);
+double gun_range(Race *race, Ship *ship, int caliber);
+double tele_range(int tech_level, double base_range);
+int current_caliber(Ship *ship);
+void do_collateral(Ship *ship, int damage, int *crew_killed, int *hull_damaged,
+                   int *engine_damaged, int *fuel_lost);
+int planet_guns(long planet_id);
+int shoot_ship_to_ship(Ship *attacker, Ship *target, int strength, int range,
+                       int accuracy, char *long_msg, char *short_msg);
+int shoot_planet_to_ship(Race &race, Ship *target, int strength, char *long_msg,
+                         char *short_msg);
+int shoot_ship_to_planet(Ship *attacker, Planet &target, int strength,
+                         int range, int accuracy, SectorMap &sector_map,
+                         int sector_x, int sector_y, char *long_msg,
+                         char *short_msg);
+int hit_odds(double attacker_strength, int *attacker_guns,
+             double target_strength, int target_shields, int target_defense,
+             int target_damage, int target_crew, int target_hull,
+             int target_engine, int target_fuel, int target_mass);
+double gun_range(Race *race, Ship *ship, int caliber);
+double tele_range(int tech_level, double base_range);
+int current_caliber(Ship *ship);
+void do_collateral(Ship *ship, int damage, int *crew_killed, int *hull_damaged,
+                   int *engine_damaged, int *fuel_lost);
+int planet_guns(long planet_id);
 
 #endif  // SHOOTBLAST_H

26308834adef59343bb5790e68027c05ce0b16af
Jeff Bailey <jeffbailey@google.com>
2024-09-06 08:25:25 +0000

    [map] Remove use of global buffers
    Moved some declarations closer to definitions.

--- gb/map.cc
+++ gb/map.cc
@@ -8,16 +8,12 @@ import std.compat;
 
 #include "gb/map.h"
 
-#include "gb/buffers.h"
-
 void show_map(GameObj &g, const starnum_t snum, const planetnum_t pnum,
               const Planet &p) {
   player_t Playernum = g.player;
   governor_t Governor = g.governor;
-  player_t i;
   int iq = 0;
   char shiplocs[MAX_X][MAX_Y] = {};
-  std::ostringstream output;
 
   const int show = 1;  // TODO(jeffbailey): This was always set to on, but this
                        // fact is output to the client, which might affect the
@@ -40,93 +36,92 @@ void show_map(GameObj &g, const starnum_t snum, const planetnum_t pnum,
     }
   }
   /* report that this is a planet map */
-  output << '$';
-  output << std::format("{};", stars[snum].pnames[pnum]);
-  output << std::format("{};{};{};", p.Maxx, p.Maxy, show);
+  g.out << '$';
+  g.out << std::format("{};", stars[snum].pnames[pnum]);
+  g.out << std::format("{};{};{};", p.Maxx, p.Maxy, show);
 
   /* send map data */
   for (const auto &sector : smap) {
     bool owned1 = (sector.owner == race.governor[Governor].toggle.highlight);
     if (shiplocs[sector.x][sector.y] && iq) {
       if (race.governor[Governor].toggle.color)
-        sprintf(buf, "%c%c", (char)(sector.owner + '?'),
-                shiplocs[sector.x][sector.y]);
+        g.out << std::format("{}{}", (char)(sector.owner + '?'),
+                             shiplocs[sector.x][sector.y]);
       else {
         if (owned1 && race.governor[Governor].toggle.inverse)
-          sprintf(buf, "1%c", shiplocs[sector.x][sector.y]);
+          g.out << std::format("1{}{}", (char)(sector.owner + '?'),
+                               shiplocs[sector.x][sector.y]);
+
         else
-          sprintf(buf, "0%c", shiplocs[sector.x][sector.y]);
+          g.out << std::format("0{}{}", (char)(sector.owner + '?'),
+                               shiplocs[sector.x][sector.y]);
       }
     } else {
-      if (race.governor[Governor].toggle.color)
-        sprintf(buf, "%c%c", (char)(sector.owner + '?'),
-                desshow(Playernum, Governor, race, sector));
-      else {
-        if (owned1 && race.governor[Governor].toggle.inverse)
-          sprintf(buf, "1%c", desshow(Playernum, Governor, race, sector));
-        else
-          sprintf(buf, "0%c", desshow(Playernum, Governor, race, sector));
+      if (race.governor[Governor].toggle.color) {
+        g.out << std::format("{}{}", (char)(sector.owner + '?'),
+                             desshow(Playernum, Governor, race, sector));
+      } else {
+        if (owned1 && race.governor[Governor].toggle.inverse) {
+          g.out << std::format("1{}{}", (char)(sector.owner + '?'),
+                               shiplocs[sector.x][sector.y]);
+        } else {
+          g.out << std::format("0{}{}", (char)(sector.owner + '?'),
+                               desshow(Playernum, Governor, race, sector));
+        }
       }
     }
-    output << buf;
   }
-  output << '\n';
-  output << std::ends;
-  notify(Playernum, Governor, output.str());
+  g.out << '\n';
 
   if (show) {
-    char temp[2047];
-    sprintf(temp, "Type: %8s   Sects %7s: %3u   Aliens:", Planet_types[p.type],
-            race.Metamorph ? "covered" : "owned",
-            p.info[Playernum - 1].numsectsowned);
+    g.out << std::format(
+        "Type: {:<8}   Sects {:<7}: {:<3}   Aliens:", Planet_types[p.type],
+        race.Metamorph ? "covered" : "owned",
+        p.info[Playernum - 1].numsectsowned);
     if (p.explored || race.tech >= TECH_EXPLORE) {
       bool f = false;
-      for (i = 1; i < MAXPLAYERS; i++)
+      for (auto i = 1U; i < MAXPLAYERS; i++) {
         if (p.info[i - 1].numsectsowned != 0 && i != Playernum) {
           f = true;
-          sprintf(buf, "%c%d", isset(race.atwar, i) ? '*' : ' ', i);
-          strcat(temp, buf);
+          g.out << std::format("{}{}", isset(race.atwar, i) ? '*' : ' ', i);
         }
-      if (!f) strcat(temp, "(none)");
-    } else
-      strcat(temp, R"(???)");
-    strcat(temp, "\n");
-    notify(Playernum, Governor, temp);
-    sprintf(temp, "              Guns : %3d             Mob Points : %ld\n",
-            p.info[Playernum - 1].guns, p.info[Playernum - 1].mob_points);
-    notify(Playernum, Governor, temp);
-    sprintf(temp, "      Mobilization : %3d (%3d)     Compatibility: %.2f%%",
-            p.info[Playernum - 1].comread, p.info[Playernum - 1].mob_set,
-            p.compatibility(race));
-    if (p.conditions[TOXIC] > 50) {
-      sprintf(buf, "    (%d%% TOXIC)", p.conditions[TOXIC]);
-      strcat(temp, buf);
+      }
+      if (!f) g.out << "(none)\n";
+    } else {
+      g.out << R"(???)";
     }
-    strcat(temp, "\n");
-    notify(Playernum, Governor, temp);
-    sprintf(temp, "Resource stockpile : %-9lu    Fuel stockpile: %u\n",
-            p.info[Playernum - 1].resource, p.info[Playernum - 1].fuel);
-    notify(Playernum, Governor, temp);
-    sprintf(temp, "      Destruct cap : %-9u%18s: %-5lu (%lu/%u)\n",
-            p.info[Playernum - 1].destruct,
-            race.Metamorph ? "Tons of biomass" : "Total Population",
-            p.info[Playernum - 1].popn, p.popn,
-            round_rand(.01 * (100. - p.conditions[TOXIC]) * p.maxpopn));
-    notify(Playernum, Governor, temp);
-    sprintf(temp, "          Crystals : %-9u%18s: %-5lu (%lu)\n",
-            p.info[Playernum - 1].crystals, "Ground forces",
-            p.info[Playernum - 1].troops, p.troops);
-    notify(Playernum, Governor, temp);
-    sprintf(temp, "%ld Total Resource Deposits     Tax rate %u%%  New %u%%\n",
-            p.total_resources, p.info[Playernum - 1].tax,
-            p.info[Playernum - 1].newtax);
-    notify(Playernum, Governor, temp);
-    sprintf(temp, "Estimated Production Next Update : %.2f\n",
-            p.info[Playernum - 1].est_production);
-    notify(Playernum, Governor, temp);
+    g.out << "\n";
+    g.out << std::format(
+        "              Guns : {:<3}             Mob Points : {}\n",
+        p.info[Playernum - 1].guns, p.info[Playernum - 1].mob_points);
+    g.out << std::format(
+        "      Mobilization : {:<3} ({:<3})     Compatibility: {:.2f}%",
+        p.info[Playernum - 1].comread, p.info[Playernum - 1].mob_set,
+        p.compatibility(race));
+    if (p.conditions[TOXIC] > 50) {
+      g.out << std::format("    ({}%% TOXIC)\n", p.conditions[TOXIC]);
+    }
+    g.out << "\n";
+    g.out << std::format("Resource stockpile : {:<9}    Fuel stockpile: {}\n",
+                         p.info[Playernum - 1].resource,
+                         p.info[Playernum - 1].fuel);
+    g.out << std::format(
+        "      Destruct cap : {:<9} {:>18}: {:<5} ({:<5}/{:<})\n",
+        p.info[Playernum - 1].destruct,
+        race.Metamorph ? "Tons of biomass" : "Total Population",
+        p.info[Playernum - 1].popn, p.popn,
+        round_rand(.01 * (100. - p.conditions[TOXIC]) * p.maxpopn));
+    g.out << std::format("          Crystals : {:<9} {:>18}: {:<5} ({:<5})\n",
+                         p.info[Playernum - 1].crystals, "Ground forces",
+                         p.info[Playernum - 1].troops, p.troops);
+    g.out << std::format(
+        "{} Total Resource Deposits     Tax rate {}%%  New {}%%\n",
+        p.total_resources, p.info[Playernum - 1].tax,
+        p.info[Playernum - 1].newtax);
+    g.out << std::format("Estimated Production Next Update : {:.2f}\n",
+                         p.info[Playernum - 1].est_production);
     if (p.slaved_to) {
-      sprintf(temp, "      ENSLAVED to player %d\n", p.slaved_to);
-      notify(Playernum, Governor, temp);
+      g.out << std::format("      ENSLAVED to player {};\n", p.slaved_to);
     }
   }
 }

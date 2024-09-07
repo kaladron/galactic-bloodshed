e65715db8eced93d6d803b65c8421d346481cbc3
Jeff Bailey <jeffbailey@google.com>
2024-09-07 10:17:54 +0000

    [bombard] berserker_bombard takes a reference to ship and accepts a const race.

--- gb/bombard.cc
+++ gb/bombard.cc
@@ -35,7 +35,7 @@ import std.compat;
  * @param r The race to which the ship belongs.
  * @return The number of sectors destroyed during the bombardment.
  */
-int berserker_bombard(Ship *ship, Planet &planet, Race &r) {
+int berserker_bombard(Ship &ship, Planet &planet, const Race &r) {
   int x;
   int y;
   int x2 = -1;
@@ -47,11 +47,10 @@ int berserker_bombard(Ship *ship, Planet &planet, Race &r) {
   /* check to see if PDNs are present */
   Shiplist shiplist(planet.ships);
   for (auto s : shiplist) {
-    if (s.alive && s.type == ShipType::OTYPE_PLANDEF &&
-        s.owner != ship->owner) {
+    if (s.alive && s.type == ShipType::OTYPE_PLANDEF && s.owner != ship.owner) {
       sprintf(buf, "Bombardment of %s cancelled, PDNs are present.\n",
-              prin_ship_orbits(*ship).c_str());
-      warn(ship->owner, ship->governor, buf);
+              prin_ship_orbits(ship).c_str());
+      warn(ship.owner, ship.governor, buf);
       return 0;
     }
   }
@@ -62,11 +61,11 @@ int berserker_bombard(Ship *ship, Planet &planet, Race &r) {
   bool found = false;
   for (auto shuffled = smap.shuffle(); auto &sector_wrap : shuffled) {
     Sector &sect = sector_wrap;
-    if (sect.owner && sect.owner != ship->owner &&
+    if (sect.owner && sect.owner != ship.owner &&
         (sect.condition != SectorType::SEC_WASTED)) {
       if (isset(r.atwar, sect.owner) ||
-          (ship->type == ShipType::OTYPE_BERS &&
-           sect.owner == ship->special.mind.target)) {
+          (ship.type == ShipType::OTYPE_BERS &&
+           sect.owner == ship.special.mind.target)) {
         found = true;
         break;
       } else {
@@ -83,29 +82,29 @@ int berserker_bombard(Ship *ship, Planet &planet, Race &r) {
 
   if (!found) {
     /* there were no sectors worth bombing. */
-    if (!ship->notified) {
-      ship->notified = 1;
-      sprintf(telegram_buf, "Report from %c%lu %s\n\n", Shipltrs[ship->type],
-              ship->number, ship->name);
+    if (!ship.notified) {
+      ship.notified = 1;
+      sprintf(telegram_buf, "Report from %c%lu %s\n\n", Shipltrs[ship.type],
+              ship.number, ship.name);
       sprintf(buf, "Planet /%s/%s has been saturation bombed.\n",
-              stars[ship->storbits].name,
-              stars[ship->storbits].pnames[ship->pnumorbits]);
+              stars[ship.storbits].name,
+              stars[ship.storbits].pnames[ship.pnumorbits]);
       strcat(telegram_buf, buf);
-      notify(ship->owner, ship->governor, telegram_buf);
+      notify(ship.owner, ship.governor, telegram_buf);
     }
     return 0;
   }
 
-  int str = MIN(Shipdata[ship->type][ABIL_GUNS] * (100 - ship->damage) / 100.,
-                ship->destruct);
+  int str = MIN(Shipdata[ship.type][ABIL_GUNS] * (100 - ship.damage) / 100.,
+                ship.destruct);
   if (!str) {
     /* no weapons! */
-    if (!ship->notified) {
-      ship->notified = 1;
+    if (!ship.notified) {
+      ship.notified = 1;
       sprintf(telegram_buf,
               "Bulletin\n\n %c%lu %s has no weapons to bombard with.\n",
-              Shipltrs[ship->type], ship->number, ship->name);
-      warn(ship->owner, ship->governor, telegram_buf);
+              Shipltrs[ship.type], ship.number, ship.name);
+      warn(ship.owner, ship.governor, telegram_buf);
     }
     return 0;
   }
@@ -115,36 +114,35 @@ int berserker_bombard(Ship *ship, Planet &planet, Race &r) {
   Nuked.fill(0);
   // save owner of destroyed sector
   auto oldown = smap.get(x, y).owner;
-  ship->destruct -= str;
-  ship->mass -= str * MASS_DESTRUCT;
+  ship.destruct -= str;
+  ship.mass -= str * MASS_DESTRUCT;
 
-  auto numdest = shoot_ship_to_planet(*ship, planet, str, x, y, smap, 0, 0,
+  auto numdest = shoot_ship_to_planet(ship, planet, str, x, y, smap, 0, 0,
                                       long_buf, short_buf);
   /* (0=dont get smap) */
   if (numdest < 0) numdest = 0;
 
   /* tell the bombarding player about it.. */
   std::stringstream telegram_report;
-  telegram_report << std::format("REPORT from ship #{}\n\n", ship->number);
+  telegram_report << std::format("REPORT from ship #{}\n\n", ship.number);
   telegram_report << short_buf;
   telegram_report << std::format(
       "sector {},{} (owner {}). {} sectors destroyed.\n", x, y, oldown,
       numdest);
-  notify(ship->owner, ship->governor, telegram_report.str());
+  notify(ship.owner, ship.governor, telegram_report.str());
 
   /* notify other player. */
-  sprintf(telegram_buf, "ALERT from planet /%s/%s\n",
-          stars[ship->storbits].name,
-          stars[ship->storbits].pnames[ship->pnumorbits]);
+  sprintf(telegram_buf, "ALERT from planet /%s/%s\n", stars[ship.storbits].name,
+          stars[ship.storbits].pnames[ship.pnumorbits]);
   sprintf(buf, "%c%lu%s bombarded sector %d,%d; %d sectors destroyed.\n",
-          Shipltrs[ship->type], ship->number, ship->name, x, y, numdest);
+          Shipltrs[ship.type], ship.number, ship.name, x, y, numdest);
   strcat(telegram_buf, buf);
-  sprintf(buf, "%c%lu %s [%d] bombards %s/%s\n", Shipltrs[ship->type],
-          ship->number, ship->name, ship->owner, stars[ship->storbits].name,
-          stars[ship->storbits].pnames[ship->pnumorbits]);
+  sprintf(buf, "%c%lu %s [%d] bombards %s/%s\n", Shipltrs[ship.type],
+          ship.number, ship.name, ship.owner, stars[ship.storbits].name,
+          stars[ship.storbits].pnames[ship.pnumorbits]);
   for (player_t i = 1; i <= Num_races; i++)
-    if (Nuked[i - 1] && i != ship->owner)
-      warn(i, stars[ship->storbits].governor[i - 1], telegram_buf);
+    if (Nuked[i - 1] && i != ship.owner)
+      warn(i, stars[ship.storbits].governor[i - 1], telegram_buf);
   post(buf, NewsType::COMBAT);
 
   putsmap(smap, planet);

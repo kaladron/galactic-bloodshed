6942ef446a9671a4ab2c7cf0153527c4197bfed5
Jeff Bailey <jeffbailey@google.com>
2024-09-09 20:00:54 +0000

    [moveplanet] Fix up types (starnum_t and planetnum_t instead of ints).  Move moveplanet to module.

--- gb/planet.cc
+++ gb/planet.cc
@@ -47,3 +47,38 @@ int revolt(Planet &pl, const player_t victim, const player_t agent) {
 
   return revolted_sectors;
 }
+
+void moveplanet(const starnum_t starnum, Planet &planet,
+                const planetnum_t planetnum) {
+  if (planet.popn || planet.ships) Stinfo[starnum][planetnum].inhab = 1;
+
+  StarsInhab[starnum] = !!(stars[starnum].inhabited);
+  StarsExpl[starnum] = !!(stars[starnum].explored);
+
+  stars[starnum].inhabited = 0;
+  if (!StarsExpl[starnum]) return; /* no one's explored the star yet */
+
+  double dist = std::hypot((double)(planet.ypos), (double)(planet.xpos));
+
+  double phase = std::atan2((double)(planet.ypos), (double)(planet.xpos));
+  double period =
+      dist *
+      std::sqrt((double)(dist / (SYSTEMGRAVCONST * stars[starnum].gravity)));
+  /* keppler's law */
+
+  double xadd = dist * std::cos((double)(-1. / period + phase)) - planet.xpos;
+  double yadd = dist * std::sin((double)(-1. / period + phase)) - planet.ypos;
+  /* one update time unit - planets orbit counter-clockwise */
+
+  /* adjust ships in orbit around the planet */
+  auto sh = planet.ships;
+  while (sh) {
+    auto ship = ships[sh];
+    ship->xpos += xadd;
+    ship->ypos += yadd;
+    sh = ship->nextship;
+  }
+
+  planet.xpos += xadd;
+  planet.ypos += yadd;
+}

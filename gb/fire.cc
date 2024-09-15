b86c772f3c0d56e2e8f05c8b3b344773d8bb16da
Jeff Bailey <jeffbailey@google.com>
2024-09-15 09:56:32 +0000

    [fire] Simplify logic in check_overload
    Replaced nested conditional with a guard clause to make the function easier to read.  Added documentation to the function.

--- gb/fire.cc
+++ gb/fire.cc
@@ -20,29 +20,40 @@ bool has_planet_defense(const shipnum_t shipno, const player_t Playernum) {
   }
   return false;
 }
+
+/**
+ * @brief Checks for overload conditions on a ship's crystal and handles the
+ * consequences.
+ *
+ * @param ship The ship object to check for overload conditions.
+ * @param cew Strength of Confined Energy Weapons.
+ * @param strength A pointer to the strength value of the ship.
+ */
 void check_overload(Ship &ship, int cew, int *strength) {
-  if ((ship.laser && ship.fire_laser) || cew) {
-    if (int_rand(0, *strength) >
-        (int)((1.0 - .01 * ship.damage) * ship.tech / 2.0)) {
-      /* check to see if the ship blows up */
-      std::string message = std::format(
-          "{}: Matter-antimatter EXPLOSION from overloaded crystal on {}\n",
-          dispshiploc(ship), ship_to_string(ship));
-      kill_ship(ship.owner, &ship);
-      *strength = 0;
-      warn(ship.owner, ship.governor, message);
-      post(message, NewsType::COMBAT);
-      notify_star(ship.owner, ship.governor, ship.storbits, message);
-    } else if (int_rand(0, *strength) >
-               (int)((1.0 - .01 * ship.damage) * ship.tech / 4.0)) {
-      std::string message =
-          std::format("{}: Crystal damaged from overloading on {}.\n",
-                      dispshiploc(ship), ship_to_string(ship));
-      ship.fire_laser = 0;
-      ship.mounted = 0;
-      *strength = 0;
-      warn(ship.owner, ship.governor, message);
-    }
+  if (!(ship.laser && ship.fire_laser) && (cew == 0)) {
+    return;
+  }
+
+  // Check to see if the ship blows up
+  if (int_rand(0, *strength) >
+      (int)((1.0 - .01 * ship.damage) * ship.tech / 2.0)) {
+    std::string message = std::format(
+        "{}: Matter-antimatter EXPLOSION from overloaded crystal on {}\n",
+        dispshiploc(ship), ship_to_string(ship));
+    kill_ship(ship.owner, &ship);
+    *strength = 0;
+    warn(ship.owner, ship.governor, message);
+    post(message, NewsType::COMBAT);
+    notify_star(ship.owner, ship.governor, ship.storbits, message);
+  } else if (int_rand(0, *strength) >
+             (int)((1.0 - .01 * ship.damage) * ship.tech / 4.0)) {
+    std::string message =
+        std::format("{}: Crystal damaged from overloading on {}.\n",
+                    dispshiploc(ship), ship_to_string(ship));
+    ship.fire_laser = 0;
+    ship.mounted = 0;
+    *strength = 0;
+    warn(ship.owner, ship.governor, message);
   }
 }
 

a051a345997c94239340bf1245694bcf371d4c2e
Jeff Bailey <jeffbailey@google.com>
2024-09-26 22:04:34 +0000

    [transfer] Reduce cognitive complexity with early returns.

--- gb/commands/transfer.cc
+++ gb/commands/transfer.cc
@@ -12,7 +12,6 @@ void transfer(const command_t &argv, GameObj &g) {
   player_t Playernum = g.player;
   governor_t Governor = g.governor;
   ap_t APcount = 1;
-  player_t player;
   char commod = 0;
 
   if (g.level != ScopeLevel::LEVEL_PLAN) {
@@ -23,7 +22,8 @@ void transfer(const command_t &argv, GameObj &g) {
   if (!enufAP(Playernum, Governor, stars[g.snum].AP[Playernum - 1], APcount))
     return;
 
-  if (!(player = get_player(argv[1]))) {
+  auto player = get_player(argv[1]);
+  if (player == 0) {
     g.out << "No such player.\n";
     return;
   }
@@ -37,62 +37,63 @@ void transfer(const command_t &argv, GameObj &g) {
   std::string starplanet =
       std::format("{}/{}:", stars[g.snum].name, stars[g.snum].pnames[g.pnum]);
   switch (commod) {
-    case 'r':
+    case 'r': {
       if (give > planet.info[Playernum - 1].resource) {
         g.out << std::format("You don't have {} on this planet.\n", give);
-      } else {
-        planet.info[Playernum - 1].resource -= give;
-        planet.info[player - 1].resource += give;
-        std::string message = std::format(
-            "{} {} resources transferred from player {} to player #{}\n",
-            starplanet, give, Playernum, player);
-        g.out << message;
-        warn_race(player, message);
+        return;
       }
-      break;
+      planet.info[Playernum - 1].resource -= give;
+      planet.info[player - 1].resource += give;
+      std::string message = std::format(
+          "{} {} resources transferred from player {} to player #{}\n",
+          starplanet, give, Playernum, player);
+      g.out << message;
+      warn_race(player, message);
+    } break;
     case 'x':
-    case '&':
+    case '&': {
       if (give > planet.info[Playernum - 1].crystals) {
         g.out << std::format("You don't have {} on this planet.\n", give);
-      } else {
-        planet.info[Playernum - 1].crystals -= give;
-        planet.info[player - 1].crystals += give;
-        std::string message = std::format(
-            "{} {} crystal(s) transferred from player {} to player #{}\n",
-            starplanet, give, Playernum, player);
-        g.out << message;
-        warn_race(player, message);
+        return;
       }
-      break;
-    case 'f':
+      planet.info[Playernum - 1].crystals -= give;
+      planet.info[player - 1].crystals += give;
+      std::string message = std::format(
+          "{} {} crystal(s) transferred from player {} to player #{}\n",
+          starplanet, give, Playernum, player);
+      g.out << message;
+      warn_race(player, message);
+    } break;
+    case 'f': {
       if (give > planet.info[Playernum - 1].fuel) {
         g.out << std::format("You don't have {} fuel on this planet.\n", give);
-      } else {
-        planet.info[Playernum - 1].fuel -= give;
-        planet.info[player - 1].fuel += give;
-        std::string message =
-            std::format("{} {} fuel transferred from player {} to player #{}\n",
-                        starplanet, give, Playernum, player);
-        g.out << message;
-        warn_race(player, message);
+        return;
       }
-      break;
-    case 'd':
+      planet.info[Playernum - 1].fuel -= give;
+      planet.info[player - 1].fuel += give;
+      std::string message =
+          std::format("{} {} fuel transferred from player {} to player #{}\n",
+                      starplanet, give, Playernum, player);
+      g.out << message;
+      warn_race(player, message);
+    } break;
+    case 'd': {
       if (give > planet.info[Playernum - 1].destruct) {
         g.out << std::format("You don't have {} destruct on this planet.\n",
                              give);
-      } else {
-        planet.info[Playernum - 1].destruct -= give;
-        planet.info[player - 1].destruct += give;
-        std::string message = std::format(
-            "{} {} destruct transferred from player {} to player #{}\n",
-            starplanet, give, Playernum, player);
-        g.out << message;
-        warn_race(player, message);
+        return;
       }
-      break;
+      planet.info[Playernum - 1].destruct -= give;
+      planet.info[player - 1].destruct += give;
+      std::string message = std::format(
+          "{} {} destruct transferred from player {} to player #{}\n",
+          starplanet, give, Playernum, player);
+      g.out << message;
+      warn_race(player, message);
+    } break;
     default:
       g.out << "What?\n";
+      return;
   }
 
   putplanet(planet, stars[g.snum], g.pnum);

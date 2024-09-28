8d30f2b10a207619bc2bad533a28cb51058fbb6b
Jeff Bailey <jeffbailey@google.com>
2024-09-28 11:06:26 +0000

    [tech_status] Don't use global buf.  Instead of taking pointers in the call to tech_report_star, return a struct so it's clearer.   Combine most declarations and definitions.

--- gb/commands/tech_status.cc
+++ gb/commands/tech_status.cc
@@ -1,23 +1,25 @@
-// Copyright 2019 The Galactic Bloodshed Authors. All rights reserved.
-// Use of this source code is governed by a license that can be
-// found in the COPYING file.
+// SPDX-License-Identifier: Apache-2.0
 
 module;
 
 import gblib;
-import std.compat;
-
-#include "gb/buffers.h"
+import std;
 
 module commands;
 
 namespace {
-void tech_report_star(player_t Playernum, governor_t Governor, const Star &star,
-                      starnum_t snum, int *t_invest, double *t_gain,
-                      double *t_max_gain) {
-  char str[200];
-  double gain;
-  double max_gain;
+
+struct returns {
+  int invest = 0;
+  double gain = 0;
+  double max_gain = 0;
+};
+
+returns tech_report_star(GameObj &g, const Star &star, starnum_t snum) {
+  const player_t Playernum = g.player;
+  const governor_t Governor = g.governor;
+
+  returns totals{};
 
   if (isset(star.explored, Playernum) &&
       (!Governor || star.governor[Playernum - 1] == Governor)) {
@@ -25,66 +27,62 @@ void tech_report_star(player_t Playernum, governor_t Governor, const Star &star,
       const auto pl = getplanet(snum, i);
       if (pl.info[Playernum - 1].explored &&
           pl.info[Playernum - 1].numsectsowned) {
-        sprintf(str, "%s/%s%s", star.name, star.pnames[i],
-                (pl.info[Playernum - 1].autorep ? "*" : ""));
-        sprintf(buf, "%16.16s %10ld%10ld%8.3lf%8.3lf\n", str,
-                pl.info[Playernum - 1].popn, pl.info[Playernum - 1].tech_invest,
-                gain = tech_prod(pl.info[Playernum - 1].tech_invest,
-                                 pl.info[Playernum - 1].popn),
-                max_gain = tech_prod(pl.info[Playernum - 1].prod_res,
-                                     pl.info[Playernum - 1].popn));
-        notify(Playernum, Governor, buf);
-        *t_invest += pl.info[Playernum - 1].tech_invest;
-        *t_gain += gain;
-        *t_max_gain += max_gain;
+        std::string location =
+            std::format("{}/{}/{}", star.name, star.pnames[i],
+                        (pl.info[Playernum - 1].autorep ? "*" : ""));
+
+        auto gain = tech_prod(pl.info[Playernum - 1].tech_invest,
+                              pl.info[Playernum - 1].popn);
+        auto max_gain = tech_prod(pl.info[Playernum - 1].prod_res,
+                                  pl.info[Playernum - 1].popn);
+
+        g.out << std::format("{:16.16} {:10} {:10} {:8.3f} {:8.3f}\n", location,
+                             pl.info[Playernum - 1].popn,
+                             pl.info[Playernum - 1].tech_invest, gain,
+                             max_gain);
+        totals.invest += pl.info[Playernum - 1].tech_invest;
+        totals.gain += gain;
+        totals.max_gain += max_gain;
       }
     }
   }
+  return totals;
 }
 }  // namespace
 
 namespace GB::commands {
 void tech_status(const command_t &argv, GameObj &g) {
   const player_t Playernum = g.player;
-  const governor_t Governor = g.governor;
-  int k;
-  double total_gain = 0.0;
-  double total_max_gain = 0.0;
-  int total_invest = 0;
 
   getsdata(&Sdata);
 
-  sprintf(buf, "             ========== Technology Report ==========\n\n");
-  notify(Playernum, Governor, buf);
+  g.out << std::format(
+      "             ========== Technology Report ==========\n\n");
 
-  sprintf(buf, "       Planet          popn    invest    gain   ^gain\n");
-  notify(Playernum, Governor, buf);
+  g.out << std::format(
+      "       Planet          popn    invest    gain   ^gain\n");
 
+  returns totals{};
   if (argv.size() == 1) {
     for (starnum_t star = 0; star < Sdata.numstars; star++) {
       stars[star] = getstar(star);
-      tech_report_star(Playernum, Governor, stars[star], star, &total_invest,
-                       &total_gain, &total_max_gain);
+      totals = tech_report_star(g, stars[star], star);
     }
   } else { /* Several arguments */
-    for (k = 1; k < argv.size(); k++) {
+    for (int k = 1; k < argv.size(); k++) {
       Place where{g, argv[k]};
       if (where.err || where.level == ScopeLevel::LEVEL_UNIV ||
           where.level == ScopeLevel::LEVEL_SHIP) {
-        sprintf(buf, "Bad location `%s'.\n", argv[k].c_str());
-        notify(Playernum, Governor, buf);
+        g.out << std::format("Bad location `{}`.\n", argv[k]);
         continue;
       } /* ok, a proper location */
       starnum_t star = where.snum;
       stars[star] = getstar(star);
-      tech_report_star(Playernum, Governor, stars[star], star, &total_invest,
-                       &total_gain, &total_max_gain);
+      tech_report_star(g, stars[star], star);
     }
   }
-  sprintf(buf, "       Total Popn:  %7ld\n", Power[Playernum - 1].popn);
-  notify(Playernum, Governor, buf);
-  sprintf(buf, "Tech: %31d%8.3lf%8.3lf\n", total_invest, total_gain,
-          total_max_gain);
-  notify(Playernum, Governor, buf);
+  g.out << std::format("       Total Popn:  {:7}\n", Power[Playernum - 1].popn);
+  g.out << std::format("Tech: {:31}{:8.3f}{:8.3f}\n", totals.invest,
+                       totals.gain, totals.max_gain);
 }
 }  // namespace GB::commands

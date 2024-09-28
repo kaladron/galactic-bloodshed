af83d1856806c43dd0b168814acd7e1eb3ef19ad
Jeff Bailey <jeffbailey@google.com>
2024-09-28 11:21:09 +0000

    [star_locations] Don't use global buf and other cleanups.

--- gb/commands/star_locations.cc
+++ gb/commands/star_locations.cc
@@ -1,31 +1,22 @@
-// Copyright 2019 The Galactic Bloodshed Authors. All rights reserved.
-// Use of this source code is governed by a license that can be
-// found in the COPYING file.
+// SPDX-License-Identifier: Apache-2.0
 
 module;
 
 import gblib;
-import std.compat;
-
-#include "gb/buffers.h"
+import std;
 
 module commands;
 
 namespace GB::commands {
 void star_locations(const command_t &argv, GameObj &g) {
-  int max;
-  if (argv.size() > 1)
-    max = std::stoi(argv[1]);
-  else
-    max = 999999;
+  int max = (argv.size() > 1) ? std::stoi(argv[1]) : 999999;
 
   for (auto i = 0; i < Sdata.numstars; i++) {
     auto dist =
-        sqrt(Distsq(stars[i].xpos, stars[i].ypos, g.lastx[1], g.lasty[1]));
+        std::sqrt(Distsq(stars[i].xpos, stars[i].ypos, g.lastx[1], g.lasty[1]));
     if (std::floor(dist) <= max) {
-      sprintf(buf, "(%2d) %20.20s (%8.0f,%8.0f) %7.0f\n", i, stars[i].name,
-              stars[i].xpos, stars[i].ypos, dist);
-      notify(g.player, g.governor, buf);
+      g.out << std::format("({:2d}) {:20.20s} ({:8.0f},{:8.0f}) {:7.0f}\n", i,
+                           stars[i].name, stars[i].xpos, stars[i].ypos, dist);
     }
   }
 }

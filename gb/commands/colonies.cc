06a4ba6ce8bb1f9b75234ca847816665d399e5b4
Jeff Bailey <jeffbailey@google.com>
2025-05-28 15:53:05 +0000

    Remove copyright notice and add SPDX license identifier for clarity and compliance.

--- gb/commands/colonies.cc
+++ gb/commands/colonies.cc
@@ -1,6 +1,4 @@
-// Copyright 2019 The Galactic Bloodshed Authors. All rights reserved.
-// Use of this source code is governed by a license that can be
-// found in the COPYING file.
+// SPDX-License-Identifier: Apache-2.0
 
 module;
 

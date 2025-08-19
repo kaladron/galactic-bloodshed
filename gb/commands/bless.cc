40928a2d4f3359c2dab440feef9384971367c65f
Jeff Bailey <jeffbailey@google.com>
2025-08-19 19:26:14 +0000

    Remove remaining uses of buffers.h in commands.

--- gb/commands/bless.cc
+++ gb/commands/bless.cc
@@ -5,8 +5,6 @@ module;
 import gblib;
 import std.compat;
 
-#include "gb/buffers.h"
-
 module commands;
 
 namespace GB::commands {
@@ -42,94 +40,104 @@ void bless(const command_t &argv, GameObj &g) {
 
   if (argv[2] == "money") {
     race.governor[0].money += amount;
-    sprintf(buf, "Deity gave you %d money.\n", amount);
+    warn(who, 0, std::format("Deity gave you {} money.\n", amount));
   } else if (argv[2] == "password") {
     strcpy(race.password, argv[3].c_str());
-    sprintf(buf, "Deity changed your race password to `%s'\n", argv[3].c_str());
+    warn(who, 0,
+         std::format("Deity changed your race password to `{}`\n", argv[3]));
   } else if (argv[2] == "morale") {
     race.morale += amount;
-    sprintf(buf, "Deity gave you %d morale.\n", amount);
+    warn(who, 0, std::format("Deity gave you {} morale.\n", amount));
   } else if (argv[2] == "pods") {
     race.pods = 1;
-    sprintf(buf, "Deity gave you pod ability.\n");
+    warn(who, 0, "Deity gave you pod ability.\n");
   } else if (argv[2] == "nopods") {
     race.pods = 0;
-    sprintf(buf, "Deity took away pod ability.\n");
+    warn(who, 0, "Deity took away pod ability.\n");
   } else if (argv[2] == "collectiveiq") {
     race.collective_iq = 1;
-    sprintf(buf, "Deity gave you collective intelligence.\n");
+    warn(who, 0, "Deity gave you collective intelligence.\n");
   } else if (argv[2] == "nocollectiveiq") {
     race.collective_iq = 0;
-    sprintf(buf, "Deity took away collective intelligence.\n");
+    warn(who, 0, "Deity took away collective intelligence.\n");
   } else if (argv[2] == "maxiq") {
     race.IQ_limit = std::stoi(argv[3]);
-    sprintf(buf, "Deity gave you a maximum IQ of %d.\n", race.IQ_limit);
+    warn(who, 0,
+         std::format("Deity gave you a maximum IQ of {}.\n", race.IQ_limit));
   } else if (argv[2] == "mass") {
     race.mass = std::stof(argv[3]);
-    sprintf(buf, "Deity gave you %.2f mass.\n", race.mass);
+    warn(who, 0, std::format("Deity gave you {:.2f} mass.\n", race.mass));
   } else if (argv[2] == "metabolism") {
     race.metabolism = std::stof(argv[3]);
-    sprintf(buf, "Deity gave you %.2f metabolism.\n", race.metabolism);
+    warn(who, 0,
+         std::format("Deity gave you {:.2f} metabolism.\n", race.metabolism));
   } else if (argv[2] == "adventurism") {
     race.adventurism = std::stof(argv[3]);
-    sprintf(buf, "Deity gave you %-3.0f%% adventurism.\n",
-            race.adventurism * 100.0);
+    warn(who, 0,
+         std::format("Deity gave you {:<3.0f}% adventurism.\n",
+                     race.adventurism * 100.0));
   } else if (argv[2] == "birthrate") {
     race.birthrate = std::stof(argv[3]);
-    sprintf(buf, "Deity gave you %.2f birthrate.\n", race.birthrate);
+    warn(who, 0,
+         std::format("Deity gave you {:.2f} birthrate.\n", race.birthrate));
   } else if (argv[2] == "fertility") {
     race.fertilize = amount;
-    sprintf(buf, "Deity gave you a fetilization ability of %d.\n", amount);
+    warn(who, 0,
+         std::format("Deity gave you a fetilization ability of {}.\n", amount));
   } else if (argv[2] == "IQ") {
     race.IQ = amount;
-    sprintf(buf, "Deity gave you %d IQ.\n", amount);
+    warn(who, 0, std::format("Deity gave you {} IQ.\n", amount));
   } else if (argv[2] == "fight") {
     race.fighters = amount;
-    sprintf(buf, "Deity set your fighting ability to %d.\n", amount);
+    warn(who, 0,
+         std::format("Deity set your fighting ability to {}.\n", amount));
   } else if (argv[2] == "technology") {
     race.tech += (double)amount;
-    sprintf(buf, "Deity gave you %d technology.\n", amount);
+    warn(who, 0, std::format("Deity gave you {} technology.\n", amount));
   } else if (argv[2] == "guest") {
     race.Guest = 1;
-    sprintf(buf, "Deity turned you into a guest race.\n");
+    warn(who, 0, "Deity turned you into a guest race.\n");
   } else if (argv[2] == "god") {
     race.God = 1;
-    sprintf(buf, "Deity turned you into a deity race.\n");
+    warn(who, 0, "Deity turned you into a deity race.\n");
   } else if (argv[2] == "mortal") {
     race.God = 0;
     race.Guest = 0;
-    sprintf(buf, "Deity turned you into a mortal race.\n");
+    warn(who, 0, "Deity turned you into a mortal race.\n");
     /* sector preferences */
   } else if (argv[2] == "water") {
     race.likes[SectorType::SEC_SEA] = 0.01 * (double)amount;
-    sprintf(buf, "Deity set your water preference to %d%%\n", amount);
+    warn(who, 0,
+         std::format("Deity set your water preference to {}%\n", amount));
   } else if (argv[2] == "land") {
     race.likes[SectorType::SEC_LAND] = 0.01 * (double)amount;
-    sprintf(buf, "Deity set your land preference to %d%%\n", amount);
+    warn(who, 0,
+         std::format("Deity set your land preference to {}%\n", amount));
   } else if (argv[2] == "mountain") {
     race.likes[SectorType::SEC_MOUNT] = 0.01 * (double)amount;
-    sprintf(buf, "Deity set your mountain preference to %d%%\n", amount);
+    warn(who, 0,
+         std::format("Deity set your mountain preference to {}%\n", amount));
   } else if (argv[2] == "gas") {
     race.likes[SectorType::SEC_GAS] = 0.01 * (double)amount;
-    sprintf(buf, "Deity set your gas preference to %d%%\n", amount);
+    warn(who, 0, std::format("Deity set your gas preference to {}%\n", amount));
   } else if (argv[2] == "ice") {
     race.likes[SectorType::SEC_ICE] = 0.01 * (double)amount;
-    sprintf(buf, "Deity set your ice preference to %d%%\n", amount);
+    warn(who, 0, std::format("Deity set your ice preference to {}%\n", amount));
   } else if (argv[2] == "forest") {
     race.likes[SectorType::SEC_FOREST] = 0.01 * (double)amount;
-    sprintf(buf, "Deity set your forest preference to %d%%\n", amount);
+    warn(who, 0,
+         std::format("Deity set your forest preference to {}%\n", amount));
   } else if (argv[2] == "desert") {
     race.likes[SectorType::SEC_DESERT] = 0.01 * (double)amount;
-    sprintf(buf, "Deity set your desert preference to %d%%\n", amount);
+    warn(who, 0,
+         std::format("Deity set your desert preference to {}%\n", amount));
   } else if (argv[2] == "plated") {
     race.likes[SectorType::SEC_PLATED] = 0.01 * (double)amount;
-    sprintf(buf, "Deity set your plated preference to %d%%\n", amount);
+    warn(who, 0,
+         std::format("Deity set your plated preference to {}%\n", amount));
   } else
     Mod = 0;
-  if (Mod) {
-    putrace(race);
-    warn(who, 0, buf);
-  }
+  if (Mod) putrace(race);
   if (Mod) return;
   /* ok, must be the planet then */
   commod = argv[2][0];
@@ -139,65 +147,75 @@ void bless(const command_t &argv, GameObj &g) {
     stars[g.snum] = getstar(g.snum);
     setbit(stars[g.snum].explored(), who);
     putstar(stars[g.snum], g.snum);
-    sprintf(buf, "Deity set your explored bit at /%s/%s.\n",
-            stars[g.snum].get_name().c_str(),
-            stars[g.snum].get_planet_name(g.pnum).c_str());
+    warn(who, 0,
+         std::format("Deity set your explored bit at /{}/{}.\n",
+                     stars[g.snum].get_name(),
+                     stars[g.snum].get_planet_name(g.pnum)));
   } else if (argv[2] == "noexplorebit") {
     planet.info[who - 1].explored = 0;
-    sprintf(buf, "Deity reset your explored bit at /%s/%s.\n",
-            stars[g.snum].get_name().c_str(),
-            stars[g.snum].get_planet_name(g.pnum).c_str());
+    warn(who, 0,
+         std::format("Deity reset your explored bit at /{}/{}.\n",
+                     stars[g.snum].get_name(),
+                     stars[g.snum].get_planet_name(g.pnum)));
   } else if (argv[2] == "planetpopulation") {
     planet.info[who - 1].popn = std::stoi(argv[3]);
     planet.popn++;
-    sprintf(buf, "Deity set your population variable to %ld at /%s/%s.\n",
-            planet.info[who - 1].popn, stars[g.snum].get_name().c_str(),
-            stars[g.snum].get_planet_name(g.pnum).c_str());
+    warn(who, 0,
+         std::format("Deity set your population variable to {} at /{}/{}.\n",
+                     planet.info[who - 1].popn, stars[g.snum].get_name(),
+                     stars[g.snum].get_planet_name(g.pnum)));
   } else if (argv[2] == "inhabited") {
     stars[g.snum] = getstar(g.snum);
     setbit(stars[g.snum].inhabited(), Playernum);
     putstar(stars[g.snum], g.snum);
-    sprintf(buf, "Deity has set your inhabited bit for /%s/%s.\n",
-            stars[g.snum].get_name().c_str(),
-            stars[g.snum].get_planet_name(g.pnum).c_str());
+    warn(who, 0,
+         std::format("Deity has set your inhabited bit for /{}/{}.\n",
+                     stars[g.snum].get_name(),
+                     stars[g.snum].get_planet_name(g.pnum)));
   } else if (argv[2] == "numsectsowned") {
     planet.info[who - 1].numsectsowned = std::stoi(argv[3]);
-    sprintf(buf, "Deity set your \"numsectsowned\" variable at /%s/%s to %d.\n",
-            stars[g.snum].get_name().c_str(),
-            stars[g.snum].get_planet_name(g.pnum).c_str(),
-            planet.info[who - 1].numsectsowned);
+    warn(who, 0,
+         std::format(
+             "Deity set your \"numsectsowned\" variable at /{}/{} to {}.\n",
+             stars[g.snum].get_name(), stars[g.snum].get_planet_name(g.pnum),
+             planet.info[who - 1].numsectsowned));
   } else {
     switch (commod) {
       case 'r':
         planet.info[who - 1].resource += amount;
-        sprintf(buf, "Deity gave you %d resources at %s/%s.\n", amount,
-                stars[g.snum].get_name().c_str(),
-                stars[g.snum].get_planet_name(g.pnum).c_str());
+        warn(who, 0,
+             std::format("Deity gave you {} resources at {}/{}.\n", amount,
+                         stars[g.snum].get_name(),
+                         stars[g.snum].get_planet_name(g.pnum)));
         break;
       case 'd':
         planet.info[who - 1].destruct += amount;
-        sprintf(buf, "Deity gave you %d destruct at %s/%s.\n", amount,
-                stars[g.snum].get_name().c_str(),
-                stars[g.snum].get_planet_name(g.pnum).c_str());
+        warn(who, 0,
+             std::format("Deity gave you {} destruct at {}/{}.\n", amount,
+                         stars[g.snum].get_name(),
+                         stars[g.snum].get_planet_name(g.pnum)));
         break;
       case 'f':
         planet.info[who - 1].fuel += amount;
-        sprintf(buf, "Deity gave you %d fuel at %s/%s.\n", amount,
-                stars[g.snum].get_name().c_str(),
-                stars[g.snum].get_planet_name(g.pnum).c_str());
+        warn(who, 0,
+             std::format("Deity gave you {} fuel at {}/{}.\n", amount,
+                         stars[g.snum].get_name(),
+                         stars[g.snum].get_planet_name(g.pnum)));
         break;
       case 'x':
         planet.info[who - 1].crystals += amount;
-        sprintf(buf, "Deity gave you %d crystals at %s/%s.\n", amount,
-                stars[g.snum].get_name().c_str(),
-                stars[g.snum].get_planet_name(g.pnum).c_str());
+        warn(who, 0,
+             std::format("Deity gave you {} crystals at {}/{}.\n", amount,
+                         stars[g.snum].get_name(),
+                         stars[g.snum].get_planet_name(g.pnum)));
         break;
       case 'a':
         stars[g.snum] = getstar(g.snum);
         stars[g.snum].AP(who - 1) += amount;
         putstar(stars[g.snum], g.snum);
-        sprintf(buf, "Deity gave you %d action points at %s.\n", amount,
-                stars[g.snum].get_name().c_str());
+        warn(who, 0,
+             std::format("Deity gave you {} action points at {}.\n", amount,
+                         stars[g.snum].get_name()));
         break;
       default:
         g.out << "No such commodity.\n";
@@ -205,6 +223,5 @@ void bless(const command_t &argv, GameObj &g) {
     }
   }
   putplanet(planet, stars[g.snum], g.pnum);
-  warn_race(who, buf);
 }
 }  // namespace GB::commands
--- gb/commands/fix.cc
+++ gb/commands/fix.cc
@@ -9,8 +9,6 @@ module;
 import gblib;
 import std.compat;
 
-#include "gb/buffers.h"
-
 module commands;
 
 namespace GB::commands {
@@ -32,57 +30,66 @@ void fix(const command_t &argv, GameObj &g) {
     auto p = getplanet(g.snum, g.pnum);
     if (argv[2] == "Maxx") {
       if (argv.size() > 3) p.Maxx = std::stoi(argv[3]);
-      sprintf(buf, "Maxx = %d\n", p.Maxx);
+      notify(Playernum, Governor, std::format("Maxx = {}\n", p.Maxx));
     } else if (argv[2] == "Maxy") {
       if (argv.size() > 3) p.Maxy = std::stoi(argv[3]);
-      sprintf(buf, "Maxy = %d\n", p.Maxy);
+      notify(Playernum, Governor, std::format("Maxy = {}\n", p.Maxy));
     } else if (argv[2] == "xpos") {
       if (argv.size() > 3) p.xpos = (double)std::stoi(argv[3]);
-      sprintf(buf, "xpos = %f\n", p.xpos);
+      notify(Playernum, Governor, std::format("xpos = {}\n", p.xpos));
     } else if (argv[2] == "ypos") {
       if (argv.size() > 3) p.ypos = (double)std::stoi(argv[3]);
-      sprintf(buf, "ypos = %f\n", p.ypos);
+      notify(Playernum, Governor, std::format("ypos = {}\n", p.ypos));
     } else if (argv[2] == "ships") {
       if (argv.size() > 3) p.ships = std::stoi(argv[3]);
-      sprintf(buf, "ships = %ld\n", p.ships);
+      notify(Playernum, Governor, std::format("ships = {}\n", p.ships));
     } else if (argv[2] == "rtemp") {
       if (argv.size() > 3) p.conditions[RTEMP] = std::stoi(argv[3]);
-      sprintf(buf, "RTEMP = %d\n", p.conditions[RTEMP]);
+      notify(Playernum, Governor,
+             std::format("RTEMP = {}\n", p.conditions[RTEMP]));
     } else if (argv[2] == "temperature") {
       if (argv.size() > 3) p.conditions[TEMP] = std::stoi(argv[3]);
-      sprintf(buf, "TEMP = %d\n", p.conditions[TEMP]);
+      notify(Playernum, Governor,
+             std::format("TEMP = {}\n", p.conditions[TEMP]));
     } else if (argv[2] == "methane") {
       if (argv.size() > 3) p.conditions[METHANE] = std::stoi(argv[3]);
-      sprintf(buf, "METHANE = %d\n", p.conditions[METHANE]);
+      notify(Playernum, Governor,
+             std::format("METHANE = {}\n", p.conditions[METHANE]));
     } else if (argv[2] == "oxygen") {
       if (argv.size() > 3) p.conditions[OXYGEN] = std::stoi(argv[3]);
-      sprintf(buf, "OXYGEN = %d\n", p.conditions[OXYGEN]);
+      notify(Playernum, Governor,
+             std::format("OXYGEN = {}\n", p.conditions[OXYGEN]));
     } else if (argv[2] == "co2") {
       if (argv.size() > 3) p.conditions[CO2] = std::stoi(argv[3]);
-      sprintf(buf, "CO2 = %d\n", p.conditions[CO2]);
+      notify(Playernum, Governor, std::format("CO2 = {}\n", p.conditions[CO2]));
     } else if (argv[2] == "hydrogen") {
       if (argv.size() > 3) p.conditions[HYDROGEN] = std::stoi(argv[3]);
-      sprintf(buf, "HYDROGEN = %d\n", p.conditions[HYDROGEN]);
+      notify(Playernum, Governor,
+             std::format("HYDROGEN = {}\n", p.conditions[HYDROGEN]));
     } else if (argv[2] == "nitrogen") {
       if (argv.size() > 3) p.conditions[NITROGEN] = std::stoi(argv[3]);
-      sprintf(buf, "NITROGEN = %d\n", p.conditions[NITROGEN]);
+      notify(Playernum, Governor,
+             std::format("NITROGEN = {}\n", p.conditions[NITROGEN]));
     } else if (argv[2] == "sulfur") {
       if (argv.size() > 3) p.conditions[SULFUR] = std::stoi(argv[3]);
-      sprintf(buf, "SULFUR = %d\n", p.conditions[SULFUR]);
+      notify(Playernum, Governor,
+             std::format("SULFUR = {}\n", p.conditions[SULFUR]));
     } else if (argv[2] == "helium") {
       if (argv.size() > 3) p.conditions[HELIUM] = std::stoi(argv[3]);
-      sprintf(buf, "HELIUM = %d\n", p.conditions[HELIUM]);
+      notify(Playernum, Governor,
+             std::format("HELIUM = {}\n", p.conditions[HELIUM]));
     } else if (argv[2] == "other") {
       if (argv.size() > 3) p.conditions[OTHER] = std::stoi(argv[3]);
-      sprintf(buf, "OTHER = %d\n", p.conditions[OTHER]);
+      notify(Playernum, Governor,
+             std::format("OTHER = {}\n", p.conditions[OTHER]));
     } else if (argv[2] == "toxic") {
       if (argv.size() > 3) p.conditions[TOXIC] = std::stoi(argv[3]);
-      sprintf(buf, "TOXIC = %d\n", p.conditions[TOXIC]);
+      notify(Playernum, Governor,
+             std::format("TOXIC = {}\n", p.conditions[TOXIC]));
     } else {
       g.out << "No such option for 'fix planet'.\n";
       return;
     }
-    notify(Playernum, Governor, buf);
     if (argv.size() > 3) putplanet(p, stars[g.snum], g.pnum);
     return;
   }
@@ -95,32 +102,33 @@ void fix(const command_t &argv, GameObj &g) {
     auto s = getship(g.shipno);
     if (argv[2] == "fuel") {
       if (argv.size() > 3) s->fuel = (double)std::stoi(argv[3]);
-      sprintf(buf, "fuel = %f\n", s->fuel);
+      notify(Playernum, Governor, std::format("fuel = {}\n", s->fuel));
     } else if (argv[2] == "max_fuel") {
       if (argv.size() > 3) s->max_fuel = std::stoi(argv[3]);
-      sprintf(buf, "fuel = %d\n", s->max_fuel);
+      notify(Playernum, Governor, std::format("fuel = {}\n", s->max_fuel));
     } else if (argv[2] == "destruct") {
       if (argv.size() > 3) s->destruct = std::stoi(argv[3]);
-      sprintf(buf, "destruct = %d\n", s->destruct);
+      notify(Playernum, Governor, std::format("destruct = {}\n", s->destruct));
     } else if (argv[2] == "resource") {
       if (argv.size() > 3) s->resource = std::stoi(argv[3]);
-      sprintf(buf, "resource = %lu\n", s->resource);
+      notify(Playernum, Governor, std::format("resource = {}\n", s->resource));
     } else if (argv[2] == "damage") {
       if (argv.size() > 3) s->damage = std::stoi(argv[3]);
-      sprintf(buf, "damage = %d\n", s->damage);
+      notify(Playernum, Governor, std::format("damage = {}\n", s->damage));
     } else if (argv[2] == "alive") {
       s->alive = 1;
       s->damage = 0;
-      sprintf(buf, "%s resurrected\n", ship_to_string(*s).c_str());
+      notify(Playernum, Governor,
+             std::format("{} resurrected\n", ship_to_string(*s)));
     } else if (argv[2] == "dead") {
       s->alive = 0;
       s->damage = 100;
-      sprintf(buf, "%s destroyed\n", ship_to_string(*s).c_str());
+      notify(Playernum, Governor,
+             std::format("{} destroyed\n", ship_to_string(*s)));
     } else {
       g.out << "No such option for 'fix ship'.\n";
       return;
     }
-    notify(Playernum, Governor, buf);
     putship(*s);
     return;
   }

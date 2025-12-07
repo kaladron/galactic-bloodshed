068ef2e2c8d78c621e67cd611dd394b36006f531
Jeff Bailey <jeffbailey@google.com>
2025-12-07 12:07:13 +0000

    chore: update license header to SPDX format in survey.cc

--- gb/commands/survey.cc
+++ gb/commands/survey.cc
@@ -1,8 +1,4 @@
-// Copyright 2014 The Galactic Bloodshed Authors. All rights reserved.
-// Use of this source code is governed by a license that can be
-// found in the COPYING file.
-
-/* survey.c -- print out survey for planets */
+// SPDX-License-Identifier: Apache-2.0
 
 module;
 

801ff250683e0d35bd6c51db26f4d059340e0984
Jeff Bailey <jeffbailey@google.com>
2025-12-25 21:17:38 +0000

    refactor: Convert block command to use tabulate
    - Replace multi-line std::format output with two tabulate tables
    - Block member power report table
    - Alliance blocks summary table
    - Rename dummy_ to found_any bool for clarity
    - Switch from std.compat to std

--- gb/commands/block.cc
+++ gb/commands/block.cc
@@ -5,7 +5,8 @@
 module;
 
 import gblib;
-import std.compat;
+import std;
+import tabulate;
 
 module commands;
 
@@ -13,11 +14,9 @@ namespace GB::commands {
 void block(const command_t& argv, GameObj& g) {
   player_t Playernum = g.player;
   governor_t Governor = g.governor;
-  // TODO(jeffbailey): ap_t APcount = 0;
   player_t p;
-  int dummy_;
 
-  const auto* race = g.race;  // Use pre-populated race from GameObj
+  const auto* race = g.race;
 
   if (argv.size() == 3 && argv[1] == "player") {
     if (!(p = get_player(g.entity_manager, argv[2]))) {
@@ -29,47 +28,48 @@ void block(const command_t& argv, GameObj& g) {
       g.out << "Race not found.\n";
       return;
     }
-    dummy_ = 0; /* Used as flag for finding a block */
+    // Flag for finding a block
+    bool found_any = false;
     g.out << std::format("Race #{} [{}] is a member of ", p, r->name);
     for (int i = 1; i <= g.entity_manager.num_races(); i++) {
       const auto* block_i = g.entity_manager.peek_block(i);
       if (!block_i) continue;
       if (isset(block_i->pledge, p) && isset(block_i->invite, p)) {
-        g.out << std::format("{}{}", (dummy_ == 0) ? " " : ", ", i);
-        dummy_ = 1;
+        g.out << std::format("{}{}", found_any ? ", " : " ", i);
+        found_any = true;
       }
     }
-    if (dummy_ == 0)
+    if (!found_any)
       g.out << "no blocks\n";
     else
       g.out << "\n";
 
-    dummy_ = 0; /* Used as flag for finding a block */
+    found_any = false;
     g.out << std::format("Race #{} [{}] has been invited to join ", p, r->name);
     for (int i = 1; i <= g.entity_manager.num_races(); i++) {
       const auto* block_i = g.entity_manager.peek_block(i);
       if (!block_i) continue;
       if (!isset(block_i->pledge, p) && isset(block_i->invite, p)) {
-        g.out << std::format("{}{}", (dummy_ == 0) ? " " : ", ", i);
-        dummy_ = 1;
+        g.out << std::format("{}{}", found_any ? ", " : " ", i);
+        found_any = true;
       }
     }
-    if (dummy_ == 0)
+    if (!found_any)
       g.out << "no blocks\n";
     else
       g.out << "\n";
 
-    dummy_ = 0; /* Used as flag for finding a block */
+    found_any = false;
     g.out << std::format("Race #{} [{}] has pledged ", p, r->name);
     for (int i = 1; i <= g.entity_manager.num_races(); i++) {
       const auto* block_i = g.entity_manager.peek_block(i);
       if (!block_i) continue;
       if (isset(block_i->pledge, p) && !isset(block_i->invite, p)) {
-        g.out << std::format("{}{}", (dummy_ == 0) ? " " : ", ", i);
-        dummy_ = 1;
+        g.out << std::format("{}{}", found_any ? ", " : " ", i);
+        found_any = true;
       }
     }
-    if (!dummy_)
+    if (!found_any)
       g.out << "no blocks\n";
     else
       g.out << "\n";
@@ -84,12 +84,29 @@ void block(const command_t& argv, GameObj& g) {
       g.out << "Block not found.\n";
       return;
     }
-    uint64_t allied_members = (block_p->invite & block_p->pledge);
+    std::uint64_t allied_members = (block_p->invite & block_p->pledge);
     g.out << std::format("         ========== {} Power Report ==========\n",
                          block_p->name);
     g.out << std::format("                 {:<64.64}\n", block_p->motto);
-    g.out << "  #  Name              troops  pop  money ship  plan  res fuel "
-             "dest know\n";
+
+    tabulate::Table table;
+    table.format().hide_border().column_separator("  ");
+
+    table.column(0).format().width(2).font_align(tabulate::FontAlign::right);
+    table.column(1).format().width(20);
+    table.column(2).format().width(6).font_align(tabulate::FontAlign::right);
+    table.column(3).format().width(5).font_align(tabulate::FontAlign::right);
+    table.column(4).format().width(5).font_align(tabulate::FontAlign::right);
+    table.column(5).format().width(5).font_align(tabulate::FontAlign::right);
+    table.column(6).format().width(5).font_align(tabulate::FontAlign::right);
+    table.column(7).format().width(5).font_align(tabulate::FontAlign::right);
+    table.column(8).format().width(5).font_align(tabulate::FontAlign::right);
+    table.column(9).format().width(5).font_align(tabulate::FontAlign::right);
+    table.column(10).format().width(4).font_align(tabulate::FontAlign::right);
+
+    table.add_row({"#", "Name", "troops", "pop", "money", "ship", "plan", "res",
+                   "fuel", "dest", "know"});
+    table[0].format().font_style({tabulate::FontStyle::bold});
 
     for (player_t i = 1; i <= g.entity_manager.num_races(); i++) {
       if (!isset(allied_members, i)) continue;
@@ -97,57 +114,65 @@ void block(const command_t& argv, GameObj& g) {
       if (!r || r->dissolved) continue;
       const auto* power_ptr = g.entity_manager.peek_power(r->Playernum);
       if (!power_ptr) continue;
-      g.out << std::format("{:2d} {:<20.20s} ", r->Playernum, r->name);
-      g.out << std::format("{:5s}",
-                           Estimate_i(power_ptr->troops, *race, r->Playernum));
-      g.out << std::format("{:5s}",
-                           Estimate_i(power_ptr->popn, *race, r->Playernum));
-      g.out << std::format("{:5s}",
-                           Estimate_i(power_ptr->money, *race, r->Playernum));
-      g.out << std::format(
-          "{:5s}", Estimate_i(power_ptr->ships_owned, *race, r->Playernum));
-      g.out << std::format(
-          "{:5s}", Estimate_i(power_ptr->planets_owned, *race, r->Playernum));
-      g.out << std::format(
-          "{:5s}", Estimate_i(power_ptr->resource, *race, r->Playernum));
-      g.out << std::format("{:5s}",
-                           Estimate_i(power_ptr->fuel, *race, r->Playernum));
-      g.out << std::format(
-          "{:5s}", Estimate_i(power_ptr->destruct, *race, r->Playernum));
-      g.out << std::format(" {:3d}%%\n", race->translate[r->Playernum - 1]);
+
+      table.add_row({std::format("{}", r->Playernum), std::string(r->name),
+                     Estimate_i(power_ptr->troops, *race, r->Playernum),
+                     Estimate_i(power_ptr->popn, *race, r->Playernum),
+                     Estimate_i(power_ptr->money, *race, r->Playernum),
+                     Estimate_i(power_ptr->ships_owned, *race, r->Playernum),
+                     Estimate_i(power_ptr->planets_owned, *race, r->Playernum),
+                     Estimate_i(power_ptr->resource, *race, r->Playernum),
+                     Estimate_i(power_ptr->fuel, *race, r->Playernum),
+                     Estimate_i(power_ptr->destruct, *race, r->Playernum),
+                     std::format("{}%", race->translate[r->Playernum - 1])});
     }
-  } else { /* list power report for all the alliance blocks (as of the last
-              update) */
+
+    g.out << table << "\n";
+  } else {
+    /* list power report for all the alliance blocks (as of the last update) */
     g.out << std::format(
         "         ========== Alliance Blocks as of {} ==========\n",
         std::asctime(std::localtime(&Power_blocks.time)));
-    g.out << " #  Name             memb money popn ship  sys  res fuel dest  VPs "
-             "know\n";
+
+    tabulate::Table table;
+    table.format().hide_border().column_separator("  ");
+
+    table.column(0).format().width(2).font_align(tabulate::FontAlign::right);
+    table.column(1).format().width(19);
+    table.column(2).format().width(4).font_align(tabulate::FontAlign::right);
+    table.column(3).format().width(5).font_align(tabulate::FontAlign::right);
+    table.column(4).format().width(5).font_align(tabulate::FontAlign::right);
+    table.column(5).format().width(5).font_align(tabulate::FontAlign::right);
+    table.column(6).format().width(5).font_align(tabulate::FontAlign::right);
+    table.column(7).format().width(5).font_align(tabulate::FontAlign::right);
+    table.column(8).format().width(5).font_align(tabulate::FontAlign::right);
+    table.column(9).format().width(5).font_align(tabulate::FontAlign::right);
+    table.column(10).format().width(5).font_align(tabulate::FontAlign::right);
+    table.column(11).format().width(4).font_align(tabulate::FontAlign::right);
+
+    table.add_row({"#", "Name", "memb", "money", "popn", "ship", "sys", "res",
+                   "fuel", "dest", "VPs", "know"});
+    table[0].format().font_style({tabulate::FontStyle::bold});
+
     for (auto i = 1; i <= g.entity_manager.num_races(); i++) {
       const auto* block_i = g.entity_manager.peek_block(i);
       if (!block_i || !block_i->VPs) continue;
-      g.out << std::format("{:2d} {:<19.19}{:3d}", i, block_i->name,
-                           Power_blocks.members[i - 1]);
-      g.out << std::format(
-          "{:5s}", Estimate_i((int)(Power_blocks.money[i - 1]), *race, i));
-      g.out << std::format(
-          "{:5s}", Estimate_i((int)(Power_blocks.popn[i - 1]), *race, i));
-      g.out << std::format(
-          "{:5s}",
-          Estimate_i((int)(Power_blocks.ships_owned[i - 1]), *race, i));
-      g.out << std::format(
-          "{:5s}",
-          Estimate_i((int)(Power_blocks.systems_owned[i - 1]), *race, i));
-      g.out << std::format(
-          "{:5s}", Estimate_i((int)(Power_blocks.resource[i - 1]), *race, i));
-      g.out << std::format(
-          "{:5s}", Estimate_i((int)(Power_blocks.fuel[i - 1]), *race, i));
-      g.out << std::format(
-          "{:5s}", Estimate_i((int)(Power_blocks.destruct[i - 1]), *race, i));
-      g.out << std::format(
-          "{:5s}", Estimate_i((int)(Power_blocks.VPs[i - 1]), *race, i));
-      g.out << std::format(" {:3d}%%\n", race->translate[i - 1]);
+
+      table.add_row(
+          {std::format("{}", i), std::string(block_i->name),
+           std::format("{}", Power_blocks.members[i - 1]),
+           Estimate_i((int)(Power_blocks.money[i - 1]), *race, i),
+           Estimate_i((int)(Power_blocks.popn[i - 1]), *race, i),
+           Estimate_i((int)(Power_blocks.ships_owned[i - 1]), *race, i),
+           Estimate_i((int)(Power_blocks.systems_owned[i - 1]), *race, i),
+           Estimate_i((int)(Power_blocks.resource[i - 1]), *race, i),
+           Estimate_i((int)(Power_blocks.fuel[i - 1]), *race, i),
+           Estimate_i((int)(Power_blocks.destruct[i - 1]), *race, i),
+           Estimate_i((int)(Power_blocks.VPs[i - 1]), *race, i),
+           std::format("{}%", race->translate[i - 1])});
     }
+
+    g.out << table << "\n";
   }
 }
 }  // namespace GB::commands

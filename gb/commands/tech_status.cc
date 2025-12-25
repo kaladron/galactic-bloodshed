fc8926ae8bfdb2eb10fd6f4158feeb79c548d8d2
Jeff Bailey <jeffbailey@google.com>
2025-12-25 20:31:38 +0000

    Convert tech_status.cc to use tabulate for table output

--- gb/commands/tech_status.cc
+++ gb/commands/tech_status.cc
@@ -4,6 +4,7 @@ module;
 
 import gblib;
 import std;
+import tabulate;
 
 module commands;
 
@@ -15,16 +16,16 @@ struct returns {
   double max_gain = 0;
 };
 
-returns tech_report_star(GameObj& g, const Star& star, starnum_t snum) {
+void tech_report_star(GameObj& g, const Star& star, starnum_t snum,
+                      tabulate::Table& table, returns& totals) {
   const player_t Playernum = g.player;
   const governor_t Governor = g.governor;
 
   if (!isset(star.explored(), Playernum) ||
       (Governor && star.governor(Playernum - 1) != Governor)) {
-    return {};
+    return;
   };
 
-  returns totals{};
   for (planetnum_t i = 0; i < star.numplanets(); i++) {
     const auto* pl = g.entity_manager.peek_planet(snum, i);
     if (!pl || !pl->info(Playernum - 1).explored ||
@@ -33,7 +34,7 @@ returns tech_report_star(GameObj& g, const Star& star, starnum_t snum) {
     }
 
     std::string location =
-        std::format("{}/{}/{}", star.get_name(), star.get_planet_name(i),
+        std::format("{}/{}{}", star.get_name(), star.get_planet_name(i),
                     (pl->info(Playernum - 1).autorep ? "*" : ""));
 
     auto gain = tech_prod(pl->info(Playernum - 1).tech_invest,
@@ -41,35 +42,45 @@ returns tech_report_star(GameObj& g, const Star& star, starnum_t snum) {
     auto max_gain = tech_prod(pl->info(Playernum - 1).prod_res,
                               pl->info(Playernum - 1).popn);
 
-    g.out << std::format("{:16.16} {:10} {:10} {:8.3f} {:8.3f}\n", location,
-                         pl->info(Playernum - 1).popn,
-                         pl->info(Playernum - 1).tech_invest, gain, max_gain);
+    table.add_row({location, std::format("{}", pl->info(Playernum - 1).popn),
+                   std::format("{}", pl->info(Playernum - 1).tech_invest),
+                   std::format("{:.3f}", gain), std::format("{:.3f}", max_gain)});
+
     totals.invest += pl->info(Playernum - 1).tech_invest;
     totals.gain += gain;
     totals.max_gain += max_gain;
   }
-  return totals;
 }
 }  // namespace
 
-// FIXME(jeffbailey): Use tabulate here.
 namespace GB::commands {
 void tech_status(const command_t& argv, GameObj& g) {
   const player_t Playernum = g.player;
 
   const auto& sdata = *g.entity_manager.peek_universe();
 
-  g.out << std::format(
-      "             ========== Technology Report ==========\n\n");
+  g.out << "             ========== Technology Report ==========\n\n";
 
-  g.out << std::format(
-      "       Planet          popn    invest    gain   ^gain\n");
+  // Create table
+  tabulate::Table table;
+  table.format().hide_border().column_separator("  ");
+
+  // Configure columns
+  table.column(0).format().width(16);  // Planet
+  table.column(1).format().width(10).font_align(tabulate::FontAlign::right);  // popn
+  table.column(2).format().width(10).font_align(tabulate::FontAlign::right);  // invest
+  table.column(3).format().width(8).font_align(tabulate::FontAlign::right);   // gain
+  table.column(4).format().width(8).font_align(tabulate::FontAlign::right);   // ^gain
+
+  // Add header
+  table.add_row({"Planet", "popn", "invest", "gain", "^gain"});
+  table[0].format().font_style({tabulate::FontStyle::bold});
 
   returns totals{};
   if (argv.size() == 1) {
     for (auto star_handle : StarList(g.entity_manager)) {
       const auto& star = *star_handle;
-      totals = tech_report_star(g, star, star.star_id());
+      tech_report_star(g, star, star.star_id(), table, totals);
     }
   } else { /* Several arguments */
     for (int k = 1; k < argv.size(); k++) {
@@ -83,9 +94,12 @@ void tech_status(const command_t& argv, GameObj& g) {
       const auto* star_ptr = g.entity_manager.peek_star(star);
       if (!star_ptr) continue;
       Star star_wrapper(*star_ptr);
-      tech_report_star(g, star_wrapper, star);
+      tech_report_star(g, star_wrapper, star, table, totals);
     }
   }
+
+  g.out << table << "\n";
+
   const auto* power_ptr = g.entity_manager.peek_power(Playernum);
   if (!power_ptr) {
     g.out << "       Total Popn:  unknown\n";

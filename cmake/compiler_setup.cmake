cd67394aee2dd47a27bb18bbbeece80781a08d43
Jeff Bailey <jeffbailey@google.com>
2026-08-23 14:15:53 +0100

    ci: add code coverage job using llvm-cov and ENABLE_COVERAGE option
    - Add ENABLE_COVERAGE CMake option to instrument builds with llvm profiling
    - Add coverage job to GitHub Actions workflow with GITHUB_STEP_SUMMARY report

--- cmake/compiler_setup.cmake
+++ cmake/compiler_setup.cmake
@@ -43,3 +43,13 @@ set(CMAKE_LINKER_FLAGS_DEBUG
 
 # Compiler options
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Wno-sign-compare -Wdocumentation -Wno-missing-designated-field-initializers")
+
+# Coverage support
+option(ENABLE_COVERAGE "Enable code coverage testing" OFF)
+if(ENABLE_COVERAGE)
+    message(STATUS "Enabling code coverage instrumentation")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fprofile-instr-generate -fcoverage-mapping")
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fprofile-instr-generate")
+    set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -fprofile-instr-generate")
+    set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} -fprofile-instr-generate")
+endif()
